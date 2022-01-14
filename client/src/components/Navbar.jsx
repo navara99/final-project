91e0727ff951b6434111fbc35141dc4986e83801
Siu <siuc@protonmail.com>
2022-01-13 20:02:11 -0500

    make new css file for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import "./Navbar.css";
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <FontAwesomeIcon icon={faCoffee} />
+      <Link to="/">
+        <FontAwesomeIcon icon={faCoffee} />
+      </Link>
     </div>
   );
 }
