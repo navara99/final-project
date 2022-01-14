63bad5092f5b59b04e1577a9457b9312601d38d8
Siu <siuc@protonmail.com>
2022-01-13 19:52:56 -0500

    install react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
-import axios from "axios"
-import { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Dashboard from './components/Home/Dashboard';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Home/Dashboard";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-
-  useEffect(()=> {
-
-    axios.get("/test").then((result)=> {
-      console.log(result)
-    })
-
-  }, [])
+  useEffect(() => {
+    axios.get("/test").then((result) => {
+      console.log(result);
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <Navbar/>
-      <Dashboard/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Dashboard />
+      </div>
+    </BrowserRouter>
   );
 }
 
