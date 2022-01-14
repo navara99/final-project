0ec862c312b28b9adc9b5711f44351ff79339a2a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-14 17:14:11 -0500

    Add list component for companies

---
+++ client/src/components/CareerFair/Companies/CompanyList.jsx
@@ -0,0 +1,17 @@
+import { Grid } from '@mui/material';
+import React from 'react'
+import CompanyItem from './CompanyItem';
+
+const CompanyList = (props) => {
+  const {industries} = props;
+  console.log(industries);
+  let industriesList = industries ? industries.map(industry => <CompanyItem key={industry.name} industry={industry}/>) : null;
+
+  return (
+    <>
+     {industriesList}
+    </>
+  )
+}
+
+export default CompanyList
