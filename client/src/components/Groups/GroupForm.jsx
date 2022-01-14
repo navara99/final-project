cac999a586440f3b657e9ff3f3962046861c8c5a
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 17:18:26 -0500

    setup creating organization form

--- client/src/components/Groups/GroupForm.jsx
+++ client/src/components/Groups/GroupForm.jsx
@@ -1,7 +1,69 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+  Button
+} from "@mui/material";
+import React from "react";
+
+function GroupForm({ openForm, setOpenForm }) {
 
 
-function GroupForm() {
-
+  return (
+    <Dialog open={openForm} onClose={() => { }}>
+      <DialogTitle>Create New Organization</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Add your organization's info
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Organization Name"
+          fullWidth
+          required
+        />
+        <TextField
+          multiline
+          rows="4"
+          margin="dense"
+          id="description"
+          label="Description"
+          fullWidth
+          required
+        />
+        <TextField
+          margin="dense"
+          id="email"
+          label="Email"
+          fullWidth
+          type="email"
+          required
+        />
+        <TextField
+          margin="dense"
+          id="industry"
+          label="Industry"
+          fullWidth
+          required
+        />
+        <TextField
+          margin="dense"
+          id="website"
+          label="Website"
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => { setOpenForm(!openForm) }}>Cancel</Button>
+        <Button onClick={() => { }}>Create</Button>
+      </DialogActions>
+    </Dialog>
+  )
 };
 
 export default GroupForm;
--- client/src/components/Groups/index.jsx
+++ client/src/components/Groups/index.jsx
@@ -3,6 +3,7 @@ import { List, Button } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import "./groups.css";
 import GroupListItem from "./GroupListItem";
+import GroupForm from "./GroupForm";
 import { useState } from "react";
 
 function Groups() {
@@ -15,6 +16,7 @@ function Groups() {
       <div className="organization-header">
         <h1>Your Organizations</h1>
         <Button variant="contained" startIcon={<Add />} onClick={() => setOpenForm(!openForm)}>Add Organization</Button>
+        <GroupForm {...{ openForm }} {...{ setOpenForm }} />
       </div>
       <List sx={{ width: '90%', paddingTop: "2em" }}>
         <GroupListItem />
