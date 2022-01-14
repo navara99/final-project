d5acfd927a40aeed38a326e6dde4d7290a1d839d
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-14 10:27:25 -0500

    Add user routes middleware (future)

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,17 +9,18 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.post("/", (req, res) => {
+      console.log("body", req.body)
+    // db.query(`SELECT * FROM users;`)
+      // .then(data => {
+      //   const users = data.rows;
+      //   res.json({ users });
+      // })
+      // .catch(err => {
+      //   res
+      //     .status(500)
+      //     .json({ error: err.message });
+      // });
   });
   return router;
 };
