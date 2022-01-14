56d2ec666790dc64da8abf245052bf24b3a09725
Siu <siuc@protonmail.com>
2022-01-13 20:38:43 -0500

    replace all link in mainbutton with button

--- client/src/components/Navbar/MainButtons.jsx
+++ client/src/components/Navbar/MainButtons.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faThList } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
 
@@ -8,9 +6,7 @@ const MainButtons = () => {
   return (
     <div>
       <Button to="/" icon={faHome}/>
-      <Link to="/jobs">
-        <FontAwesomeIcon icon={faThList} />
-      </Link>
+      <Button to="/jobs" icon={faThList}/>
     </div>
   );
 };
