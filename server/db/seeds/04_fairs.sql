c90013d67364e36f6fed8155db3f3206473d3c24
Siu <siuc@protonmail.com>
2022-01-14 11:24:42 -0500

    format seeds for fairs

--- server/db/seeds/04_fairs.sql
+++ server/db/seeds/04_fairs.sql
@@ -1,10 +1,12 @@
-copy fairs (
+INSERT INTO  fairs (
   name,
   host_id,
   description,
   start_time,
   end_time,
   poster
-) FROM '/Users/yagneshparekh/lighthouse/final-project/server/db/raw_data/fairs.csv' -- replace path for related .csv file from your folder
-DELIMITER ','
-CSV HEADER;
+) 
+VALUES
+('Lindgren Shields and Champlin', 1, 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo rhoncus sed vestibulum sit amet cursus id, turpis. Integer aliquet massa id lobortis convallis, tortor risus dapibus augue vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.', '1/14/2022', '1/19/2022', 'http://dummyimage.com/244x100.png/dddddd/000000'),
+('Frami-Kuhn', 2, 'Donec dapibus.', '1/14/2022', '1/19/2022', 'http://dummyimage.com/249x100.png/ff4444/ffffff')
+;
