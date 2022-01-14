1fe911c8be80bcf007b90a7f8712ad79c30826eb
Siu <siuc@protonmail.com>
2022-01-13 21:58:47 -0500

    add user buttons

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Navbar.css";
 import MainButtons from "./MainButtons";
 import NullUserButtons from "./NullUserButtons"
+import UserButtons from "./UserButtons";
 
 const Navbar = ({ currentUser }) => {
   return (
     <div className="navbar">
       <MainButtons />
       {!currentUser && <NullUserButtons />}
+      {currentUser && <UserButtons currentUser={currentUser} />}
     </div>
   );
 };
