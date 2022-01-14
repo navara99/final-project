f3ccf2f62270f5bb8638a39067e224d3189f63f5
Siu <siuc@protonmail.com>
2022-01-13 20:43:48 -0500

    refactor nullUserButtons

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import "./Navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignInAlt, faRegistered } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import MainButtons from "./MainButtons";
+import NullUserButtons from "./NullUserButtons"
 
 const Navbar = ({ currentUser }) => {
   return (
     <div className="navbar">
       <MainButtons />
-      <div>
-        <Link to="/login">
-          <FontAwesomeIcon icon={faSignInAlt} />
-        </Link>
-        <Link to="/register">
-          <FontAwesomeIcon icon={faRegistered} />
-        </Link>
-      </div>
+      {!currentUser && <NullUserButtons />}
     </div>
   );
 };
