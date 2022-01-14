05ed708c6c1cac193f08c19b7e4b9a1853e961a4
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-14 10:27:55 -0500

    Add login route for user

--- server/server.js
+++ server/server.js
@@ -14,13 +14,33 @@ const db = new Pool(dbParams);
 db.connect();
 
 app.use(morgan("dev"));
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static("public"));
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
+
+//login route
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  // find user with email
+  const data = await db.query('SELECT * FROM users WHERE email = $1;',[`${email}`]);
+  const user = data.rows[0];
+    //if user not there
+    if (!user) {
+      return res.status(401).json({message : "Email doesn't exists"});
+    }
+    // // compare provided password
+    if(user.password !== password) {
+      return res.status(401).json({message : "Password doesn't match"});
+    }
+    // // assign session_id
+    // req.session.user_id = user.id;
+    res.status(200).json(user);
+})
+
 app.use("/test", (req, res) => {
   res.json({
     status:"success"
@@ -30,6 +50,8 @@ app.use("/test", (req, res) => {
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
