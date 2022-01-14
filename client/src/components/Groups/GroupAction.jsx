e6a36bdf49fe70f793946f77be31baeb924efb50
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 16:12:50 -0500

    add icons to btn action btn

--- client/src/components/Groups/GroupAction.jsx
+++ client/src/components/Groups/GroupAction.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import GroupsBtn from "./GroupsBtn";
+import { Delete, Edit, Add } from "@mui/icons-material";
 
 function GroupAction() {
-
-
 
   const btnInfo = [
     {
       text: "Add Member",
       variant: "contained",
+      icon: <Add />
     },
     {
       text: "Edit",
-      variant: "outlined"
+      variant: "outlined",
+      icon: <Edit />
     }
     ,
     {
       text: "Delete",
-      variant: "outlined"
+      variant: "outlined",
+      icon: <Delete />
     }
   ];
 
   const btns = btnInfo.map((info, i) => {
-    return <GroupsBtn key={i} text={info.text} variant={info.variant} />
+    return <GroupsBtn key={i} text={info.text} variant={info.variant} icon={info.icon} />
   });
-
 
   return (
     <div className="organization-action-btn">
--- client/src/components/Groups/GroupsBtn.jsx
+++ client/src/components/Groups/GroupsBtn.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Button } from "@mui/material";
 import "./groups.css";
 
-function GroupsBtn({ text, variant }) {
+function GroupsBtn({ text, variant, icon }) {
 
 
   return (
-    <Button variant={variant} style={{ margin: "0.8em 0.25em" }}>{text}</Button>
+    <Button variant={variant} style={{ margin: "0.8em 0.25em" }} startIcon={icon}>{text}</Button>
   );
 };
 
