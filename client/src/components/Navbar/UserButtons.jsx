5e5579a904e84889b34950394fa630d394c64c9e
Siu <siuc@protonmail.com>
2022-01-14 14:39:30 -0500

    Update UserButtons.jsx

--- client/src/components/Navbar/UserButtons.jsx
+++ client/src/components/Navbar/UserButtons.jsx
@@ -4,18 +4,24 @@ import DateRangeIcon from "@mui/icons-material/DateRange";
 import GroupsIcon from "@mui/icons-material/Groups";
 import ForumIcon from "@mui/icons-material/Forum";
 import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from '@mui/icons-material/Logout';
+import LogoutIcon from "@mui/icons-material/Logout";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 const UserButtons = ({ logout, currentUser }) => {
   return (
     <div>
-      <Button {...{ to: "/schedule", Icon: DateRangeIcon, text: "My Schedule" }} />
+      <Button
+        {...{ to: "/schedule", Icon: DateRangeIcon, text: "My Schedule" }}
+      />
       <Button {...{ to: "/messages", Icon: ForumIcon, text: "Messages" }} />
       <Button {...{ to: "/groups", Icon: GroupsIcon, text: "My Groups" }} />
       <Button {...{ to: "/settings", Icon: SettingsIcon, text: "Settings" }} />
-      <span onClick={logout} className="navbar-button">
-        <LogoutIcon />
-      </span>
+      <Tippy content="Logout" placement="right" theme="material" arrow={true} >
+        <span onClick={logout} className="navbar-button">
+          <LogoutIcon />
+        </span>
+      </Tippy>
     </div>
   );
 };
