636a8b9a1360c47afe7ac6283e990dda6202a992
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 04:54:32 +0000

    add getData func

--- server/db/queryHelpers.js
+++ server/db/queryHelpers.js
@@ -1,9 +1,11 @@
+const getData = ({ rows }) => rows;
 const getFirstRecord = (result) => getData(result)[0];
+
 
 const queryGenerator = (db) => {
 
   const createNewUser = async (userInfo) => {
-
+    
   };
 
   return { createNewUser };
