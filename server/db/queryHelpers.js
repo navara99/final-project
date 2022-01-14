4894d5dcf0d434e227e7e6df6177e37099507af6
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 04:55:33 +0000

    add query in createNewUser func and return userInfo

--- server/db/queryHelpers.js
+++ server/db/queryHelpers.js
@@ -5,7 +5,22 @@ const getFirstRecord = (result) => getData(result)[0];
 const queryGenerator = (db) => {
 
   const createNewUser = async (userInfo) => {
-    
+    const { email, password, username, firstName, lastName } = userInfo;
+    const values = [email, password, username, firstName, lastName];
+    const queryString = `
+      INSERT INTO users (email, password, username, first_name, last_name)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+
+    try {
+      const result = await db.query(queryString, values);
+      const newUserInfo = getFirstRecord(result);
+
+      return newUserInfo;
+    } catch (err) {
+      console.log(err);
+    };
   };
 
   return { createNewUser };
