f8edc0907979e0b52ccd87df0721d288eb19ac00
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 23:34:51 +0000

    add user to organization they created with as admin

--- server/routes/organizations.js
+++ server/routes/organizations.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 module.exports = (db) => {
   const queryGenerator = require("../db/queryHelpers");
-  const { } = queryGenerator(db);
+  const { createNewOrganization, addUserToOrganization } = queryGenerator(db);
 
   router.post("/", async (req, res) => {
-    console.log(req.body);
+    const { user_id } = req.session;
+
+    try {
+      const newOrganization = await createNewOrganization({ ...req.body });
+      await addUserToOrganization(user_id, newOrganization.id, true);
+
+      res.json(newOrganization);
+    } catch (err) {
+      console.log(err.message);
+    };
+
   });
 
   return router;
