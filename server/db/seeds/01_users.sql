ba1be3056507199e83f986aa7a2bcdd76be6b811
Siu <siuc@protonmail.com>
2022-01-14 10:44:37 -0500

    Update 01_users.sql

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,15 +1,12 @@
--- -- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
-copy users (
+INSERT INTO users (
   first_name,
   last_name,
   email,
   profile_picture,
   password,
   username,
-  bio 
-) 
-FROM '/Users/yagneshparekh/lighthouse/final-project/server/db/raw_data/users.csv' -- replace path for related .csv file from your folder
-DELIMITER ','
-CSV HEADER;
+  bio) 
+VALUES
+('Gerrard', 'Powrie', 'gpowrie0@ow.ly', 'https://robohash.org/cupiditatedolordoloribus.png?size=50x50&set=set1', 'Ezeb5f13H90', 'gpowrie0', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.'),
+('Henrieta', 'Lehrmann', 'hlehrmann1@cbsnews.com', 'https://robohash.org/nonutvoluptas.png?size=50x50&set=set1', '1AvKfOar', 'hlehrmann1', 'Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.')
+;
