c1c8d57ec84c2fce19473dd9eca7169bad5c8ec9
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 00:37:19 -0500

    Setup error dialog modal

--- client/src/components/ErrorModal.jsx
+++ client/src/components/ErrorModal.jsx
@@ -1,7 +1,13 @@
-
+import { Dialog, DialogTitle } from "@mui/material";
 
 function ErrorModal() {
-  
+
+
+  return (
+    <Dialog open={true}>
+      <DialogTitle>Error</DialogTitle>
+    </Dialog>
+  );
 };
 
 export default ErrorModal;
