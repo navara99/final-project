2ed243d853930d0daceaad8a93b71d44422c574d
Siu <siuc@protonmail.com>
2022-01-14 11:52:03 -0500

    format the seed for jobs and applications

--- server/db/seeds/08_jobs.sql
+++ server/db/seeds/08_jobs.sql
@@ -1,9 +1,11 @@
-copy jobs (
+INSERT INTO jobs (
   name,
   created_at,
   description,
   location ,
   employer_id
-) FROM '/Users/yagneshparekh/lighthouse/final-project/server/db/raw_data/jobs.csv' -- replace path for related .csv file from your folder
-DELIMITER ','
-CSV HEADER;
+)
+VALUES
+('Budget/Accounting Analyst I', '12/11/2021', 'Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.', 'Redange-sur-Attert', 1),
+('Research Associate', '11/17/2021', 'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.', 'Changshu City', 2)
+;
