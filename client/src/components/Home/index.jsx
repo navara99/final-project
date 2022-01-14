d75fa76e02a96f259c40e32e4cf018489d546940
Siu <siuc@protonmail.com>
2022-01-14 17:14:49 -0500

    make 3 lists for fairs (past/ongoing/upcoming)

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import useFairs from "../../hooks/useFairs";
 
 const Home = () => {
-  const { onGoingFairs } = useFairs();
+  const { onGoingFairs, upcomingFairs, pastFairs } = useFairs();
 
   useEffect(() => {
+    console.log(upcomingFairs);
     console.log(onGoingFairs);
-  }, [onGoingFairs]);
+    console.log(pastFairs);
+  }, [pastFairs]);
 
   return <div>HOME</div>;
 };
--- client/src/hooks/useFairs.js
+++ client/src/hooks/useFairs.js
@@ -3,19 +3,33 @@ import axios from "axios";
 
 const useFairs = () => {
   const [onGoingFairs, setOnGoingFairs] = useState([]);
-  // const [upcomingFairs, setUpcomingFairs] = useState([]);
-  // const [pastFairs, setpastFairs] = useState([]);
+  const [upcomingFairs, setUpcomingFairs] = useState([]);
+  const [pastFairs, setpastFairs] = useState([]);
 
-  useEffect(async () => {
-    try {
-      const { data } = await axios.get("/api/fairs/");
-      setOnGoingFairs(data);
-      console.log(onGoingFairs);
-    } catch (e) {
-      console.log(e);
-    }
+  useEffect(() => {
+    axios.get("/api/fairs/").then(({ data }) => {
+      console.log(data);
+      const fairsWithValidDate = data.map((fair) => {
+        const { start_time, end_time } = fair;
+        const parsedStart = Date.parse(start_time);
+        const parsedEnd = Date.parse(end_time);
+        return { ...fair, start_time: parsedStart, end_time: parsedEnd };
+      });
+      const past = fairsWithValidDate.filter(
+        ({ end_time }) => end_time < Date.now()
+      );
+      const ongoing = fairsWithValidDate.filter(({ end_time, start_time }) => {
+        return end_time > Date.now() && start_time < Date.now();
+      });
+      const upcoming = fairsWithValidDate.filter(
+        ({ start_time }) => start_time > Date.now()
+      );
+      setOnGoingFairs(ongoing);
+      setUpcomingFairs(upcoming);
+      setpastFairs(past);
+    });
   }, []);
-  return { onGoingFairs };
+  return { onGoingFairs, upcomingFairs, pastFairs };
 };
 
 export default useFairs;
