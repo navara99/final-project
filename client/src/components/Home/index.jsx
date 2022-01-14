40c1503df61d35baa1729d432bc4290998dc2a24
Siu <siuc@protonmail.com>
2022-01-14 12:40:47 -0500

    Update index.jsx

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 const Home = () => {
-  return (
-    <div>test
-    </div>
-  )
-}
+  useEffect(() => {}, []);
+
+  return <div>test</div>;
+};
 
 export default Home;
