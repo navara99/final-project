b98a40b5b38982696d24e31c200e148db9f22e77
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 16:54:24 -0500

    refactor group list item into its own component

--- client/src/components/Groups/GroupListItem.jsx
+++ client/src/components/Groups/GroupListItem.jsx
@@ -1,8 +1,25 @@
-
+import React from "react";
+import { ListItemText, Card } from "@mui/material";
+import GroupAction from "./GroupAction";
 
 function GroupListItem() {
 
+  const cardStyles = {
+    padding: "2em",
+    margin: "1em"
+  };
 
+  const dummyDescription = "aliquet ultricies. Mauris vitae tempus odio, eget pellentesque neque. porta aliquam et id purus. Maecenas magna lorem, imperdiet at ex viverra, luctus fringilla odio. Ut a lectus dui. Nulla facilisi. Curabitur at rutrum tortor, mattis finibus turpis. Phasellus suscipit orci a justo aliquam, ut ornare leo commodo."
+
+  return (
+    <Card style={cardStyles} alignItems="flex-start">
+      <ListItemText
+        primary={<h3 className="organization-card-name">Some Organization Name</h3>}
+        secondary={dummyDescription}
+      />
+      <GroupAction />
+    </Card>
+  )
 };
 
 export default GroupListItem;
--- client/src/components/Groups/index.jsx
+++ client/src/components/Groups/index.jsx
@@ -8,15 +8,10 @@ import {
 import GroupAction from "./GroupAction";
 import { Add } from "@mui/icons-material";
 import "./groups.css";
+import GroupListItem from "./GroupListItem";
 
 function Groups() {
 
-  const cardStyles = {
-    padding: "2em",
-    margin: "1em"
-  };
-
-  const dummyDescription = "aliquet ultricies. Mauris vitae tempus odio, eget pellentesque neque. porta aliquam et id purus. Maecenas magna lorem, imperdiet at ex viverra, luctus fringilla odio. Ut a lectus dui. Nulla facilisi. Curabitur at rutrum tortor, mattis finibus turpis. Phasellus suscipit orci a justo aliquam, ut ornare leo commodo."
 
   return (
     <div className="groups-container">
@@ -25,13 +20,7 @@ function Groups() {
         <Button variant="contained" startIcon={<Add />}>Add Organization</Button>
       </div>
       <List sx={{ width: '90%', paddingTop: "2em" }}>
-        <Card style={cardStyles} alignItems="flex-start">
-          <ListItemText
-            primary={<h3 className="organization-card-name">Some Organization Name</h3>}
-            secondary={dummyDescription}
-          />
-          <GroupAction />
-        </Card>
+        <GroupListItem />
       </List>
     </div>
   )
