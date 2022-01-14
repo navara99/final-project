c2d3777eeac4f84659821230d90b0601ae5b125a
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 14:05:51 -0500

    add link to register page

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -75,14 +75,9 @@ const LogIn = ({ setCurrentUser }) => {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
+          <Grid container justifyContent="center">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/register" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
