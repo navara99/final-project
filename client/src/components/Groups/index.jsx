3a460359b8d1c64f547ed40722590665fbed05d0
Tharsikan <thar.navaratnam@gmail.com>
2022-01-14 16:57:26 -0500

    add open form state

--- client/src/components/Groups/index.jsx
+++ client/src/components/Groups/index.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import {
-  List,
-  ListItemText,
-  Button,
-  Card
-} from "@mui/material";
-import GroupAction from "./GroupAction";
+import { List, Button } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import "./groups.css";
 import GroupListItem from "./GroupListItem";
+import { useState } from "react";
 
 function Groups() {
+  const [openForm, setOpenForm] = useState(false);
+
 
 
   return (
     <div className="groups-container">
       <div className="organization-header">
         <h1>Your Organizations</h1>
-        <Button variant="contained" startIcon={<Add />}>Add Organization</Button>
+        <Button variant="contained" startIcon={<Add />} onClick={() => setOpenForm(!openForm)}>Add Organization</Button>
       </div>
       <List sx={{ width: '90%', paddingTop: "2em" }}>
         <GroupListItem />
