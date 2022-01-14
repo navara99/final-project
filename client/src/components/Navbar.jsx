11c3d46db7aa55836041c0a2200027364f6bae1d
Siu <siuc@protonmail.com>
2022-01-13 19:59:32 -0500

    install font-awesome

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
   return (
     <div className="navbar">
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
