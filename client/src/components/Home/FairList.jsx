4de28bfd410eb1fbdbc92a7a68d6f78b47151ade
Siu <siuc@protonmail.com>
2022-01-15 15:05:36 -0500

    install slick-carousel

--- client/src/components/Home/FairList.jsx
+++ client/src/components/Home/FairList.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import FairListItem from "./FairListItem";
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
 
 const objectIsEmpty = (obj) => {
   const numOfKey = Object.keys(obj).length;
@@ -11,15 +15,47 @@ const FairList = ({ text, items, showDate }) => {
   const itemArr = items.map((item) => {
     return <FairListItem {...{ ...item, key: item.id, showDate }} />;
   });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <>
-      {!objectIsEmpty(items) && (
-        <>
-          <h3>{text}</h3>
-          <div className="fair-list">{itemArr}</div>
-        </>
-      )}
-    </>
+    <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+  return (
+    // <>
+    //   {!objectIsEmpty(items) && (
+    //     <>
+    //       <h3>{text}</h3>
+    //       <div className="fair-list">{itemArr}</div>
+    //     </>
+    //   )}
+    // </>
   );
 };
 
