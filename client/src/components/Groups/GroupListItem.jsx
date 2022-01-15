9d51b9e15694c20efaf726760e97fe70ca9ad267
Tharsikan <thar.navaratnam@gmail.com>
2022-01-15 15:36:12 -0500

    add collapse to organization summary card

--- client/src/components/Groups/GroupListItem.jsx
+++ client/src/components/Groups/GroupListItem.jsx
@@ -1,28 +1,60 @@
-import React from "react";
-import { ListItemText, Card } from "@mui/material";
+import React, { useState } from "react";
+import { ListItemText, Card, IconButton, Collapse } from "@mui/material";
 import GroupAction from "./GroupAction";
+import { styled } from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function GroupListItem({ group, openAddMembersModal, setOpenAddMembersModal, selectedGroup, setSelectedGroup }) {
+  const [expanded, setExpanded] = useState(false)
 
   const cardStyles = {
     padding: "2em",
     margin: "1em"
   };
 
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Card style={cardStyles} alignItems="flex-start">
-      <ListItemText
-        primary={<h3 className="organization-card-name">{group.name}</h3>}
-        secondary={group.description}
-      />
-      <GroupAction
-        {...{ openAddMembersModal }}
-        {...{ setOpenAddMembersModal }}
-        {...{ group }}
-        {...{ selectedGroup }}
-        {...{ setSelectedGroup }}
-      />
-    </Card>
+      <div className="organization-card">
+        <ListItemText
+          primary={<h3 className="organization-card-name">{group.name}</h3>}
+        />
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </div>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <ListItemText
+          secondary={group.description}
+        />
+        <GroupAction
+          {...{ openAddMembersModal }}
+          {...{ setOpenAddMembersModal }}
+          {...{ group }}
+          {...{ selectedGroup }}
+          {...{ setSelectedGroup }}
+        />
+      </Collapse>
+    </Card >
   )
 };
 
