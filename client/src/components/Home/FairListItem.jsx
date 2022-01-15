3df19d04fd2390555a355dac7faf910949f2c892
Siu <siuc@protonmail.com>
2022-01-14 23:48:00 -0500

    add badge to card on homepage

--- client/src/components/Home/FairListItem.jsx
+++ client/src/components/Home/FairListItem.jsx
@@ -5,24 +5,19 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
 import { shortenStr } from "../../helpers/string";
-import { formatDate } from "../../helpers/date"
+import { formatDate } from "../../helpers/date";
 
-const FairListItem = ({name, description, poster, start_time, end_time}) => {
+const FairListItem = ({ name, description, poster, start_time, end_time }) => {
   return (
     <Link to="/jobs">
       <Card sx={{ maxWidth: 250 }}>
-        <CardMedia
-          component="img"
-          height="200"
-          image={poster}
-          alt={name}
-        />
+        <div className="badge">{formatDate(start_time)}</div>
+        <CardMedia component="img" height="200" image={poster} alt={name} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {formatDate(start_time)} - 
             {shortenStr(description, 120)}
           </Typography>
         </CardContent>
