231af5c40a9d9c27821af9feeff3434539ed25e8
Siu <siuc@protonmail.com>
2022-01-16 18:58:18 -0500

    update format dat string

--- client/src/components/Schedule/Event.jsx
+++ client/src/components/Schedule/Event.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
-import { formatTime } from "../../helpers/date";
+import { formatStartEndTime } from "../../helpers/date";
 
 const Event = ({ event }) => {
   const { start, end, title } = event;
-  const formatStart = formatTime(start);
-  const formatEnd = formatTime(end);
+  const time = formatStartEndTime(start, end);
+  const content = (
+    <>
+      {title}
+      <br />
+      {time}
+    </>
+  );
   return (
     <Tippy
       interactive={true}
-      content={<>{title}
-      <br />
-      {formatStart} - {formatEnd}</>}
+      content={content}
       placement="bottom"
       theme="material"
       arrow={true}
       interactiveDebounce={20}
       hideOnClick={false}
-      maxWidth="350"
+      maxWidth="150"
     >
       <div className="event">
         <strong>{event.title}</strong>
--- client/src/helpers/date.js
+++ client/src/helpers/date.js
@@ -1,13 +1,25 @@
 export const formatDate = (timestamp) => {
-  const date = (new Date(timestamp)).toDateString();
+  const date = new Date(timestamp).toDateString();
   const formatedDate = date.slice(0, -5);
   const year = date.slice(-5);
   return formatedDate + ", " + year;
 };
 
 export const formatTime = (timestamp) => {
-  const date = (new Date(timestamp)).toDateString();
-  const formatedDate = date.slice(0, -5);
-  const year = date.slice(-5);
-  return formatedDate + ", " + year;
+  const date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const time = hours + ":" + minutes + " " + ampm;
+  return time;
+};
+
+export const formatStartEndTime = (start, end) => {
+  const startTime = formatTime(start);
+  const endTime = formatTime(end);
+
+  return startTime + " - " + endTime;
 };
