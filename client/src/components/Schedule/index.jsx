3a73c9e0950a46af76d1ea4fad1c58b961eff011
Siu <siuc@protonmail.com>
2022-01-15 23:22:20 -0500

    make a blank schedule

--- client/src/components/Schedule/index.jsx
+++ client/src/components/Schedule/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./schedule.css"
+
+const localizer = momentLocalizer(moment);
 
 const Schedule = () => {
-
   return (
-    <div>My schedule</div>
+    <div className="schedule">
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
   );
 };
 
