70a01f03d652a93c628187aa1c64e094bdb9a0e7
Tharsikan <thar.navaratnam@gmail.com>
2022-01-16 03:07:34 +0000

    add another row

--- server/db/seeds/03_users_organizations.sql
+++ server/db/seeds/03_users_organizations.sql
@@ -1,5 +1,7 @@
-INSERT INTO users_organizations (user_id, organization_id, admin)
+INSERT INTO users_organizations
+  (user_id, organization_id, admin)
 VALUES
   (1, 1, true),
-  (2, 2, true)
+  (2, 2, true),
+  (1, 2, true)
 ;
