40f97f83830129d4f6c5ea69cc08f863fa03389c
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-16 07:08:27 -0500

    Add message form

---
+++ client/src/components/ChatBox/Messages/MessageListItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ListItem,Grid,ListItemText,Paper } from '@mui/material'
+const MessageListItem = ({message}) => {
+  const {text, created_at, sender_id} = message
+  return (
+    <ListItem>
+      <Grid container >
+          <Grid item xs={12} >
+              <ListItemText align={sender_id === 1 ? "right" : "left"} primary={text}></ListItemText>
+          </Grid>
+          <Grid item xs={12}>
+              <ListItemText align={sender_id === 1 ? "right" : "left"} secondary={created_at}></ListItemText>
+          </Grid>
+      </Grid>
+    </ListItem> 
+  )
+}
+
+export default MessageListItem
