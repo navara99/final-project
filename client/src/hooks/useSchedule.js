6691beabc9f66867ba33402d929973c1fcd4241b
Siu <siuc@protonmail.com>
2022-01-16 18:15:41 -0500

    make new hook for schedule

---
+++ client/src/hooks/useSchedule.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useSchedule = () => {
+  // const [onGoingFairs, setOnGoingFairs] = useState([]);
+  // const [upcomingFairs, setUpcomingFairs] = useState([]);
+  // const [pastFairs, setpastFairs] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get("/api/fairs/").then(({ data }) => {
+  //     const { past, ongoing, upcoming } = data;
+  //     setOnGoingFairs(ongoing);
+  //     setUpcomingFairs(upcoming);
+  //     setpastFairs(past);
+  //   });
+  // }, []);
+  return {  };
+};
+
+export default useSchedule;
