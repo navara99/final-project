aedbef88abe7e1889db9613a0cd81dead65f443f
Siu <siuc@protonmail.com>
2022-01-16 18:17:42 -0500

    render the data on frontend

--- client/src/hooks/useSchedule.js
+++ client/src/hooks/useSchedule.js
@@ -2,19 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useSchedule = () => {
-  // const [onGoingFairs, setOnGoingFairs] = useState([]);
-  // const [upcomingFairs, setUpcomingFairs] = useState([]);
-  // const [pastFairs, setpastFairs] = useState([]);
+  const [events, setEvents] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get("/api/fairs/").then(({ data }) => {
-  //     const { past, ongoing, upcoming } = data;
-  //     setOnGoingFairs(ongoing);
-  //     setUpcomingFairs(upcoming);
-  //     setpastFairs(past);
-  //   });
-  // }, []);
-  return {  };
+  useEffect(() => {
+    axios.get("/api/schedule/").then(({ data }) => {
+      setEvents(data);
+    });
+  }, []);
+  return events;
 };
 
 export default useSchedule;
