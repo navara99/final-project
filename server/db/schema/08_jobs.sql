d9f70809340e4bb6b26852063d33b7b184d5b7e0
Tharsikan <thar.navaratnam@gmail.com>
2022-01-16 21:48:35 +0000

    add more columns to jobs table

--- server/db/schema/08_jobs.sql
+++ server/db/schema/08_jobs.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS jobs CASCADE;
 
-CREATE TABLE jobs (
+CREATE TABLE jobs
+(
   id SERIAL PRIMARY KEY,
-  name varchar,
+  title varchar,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   description varchar NOT NULL,
+  experience varchar NOT NULL,
   location varchar NOT NULL,
-  employer_id int NOT NULL REFERENCES organizations(id) ON DELETE CASCADE
+  salary varchar NOT NULL,
+  organization_id int NOT NULL REFERENCES organizations(id) ON DELETE CASCADE
 );
