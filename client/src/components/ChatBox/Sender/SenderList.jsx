e241c05b4eec7831307d7c03985fddf44fb213cd
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-16 05:43:45 -0500

    Add structure for chatbox

---
+++ client/src/components/ChatBox/Sender/SenderList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import SenderListItem from './SenderListItem'
+import { List } from '@mui/material'
+const SenderList = () => {
+  const senders = [
+    {
+      name : 'Remy Sharp',
+      imageUrl : "https://pickaface.net/gallery/avatar/20120409_230759_3646_Fake.png",
+      last_message: "I'll be in your neighborhood doing errands this…"
+    },
+    {
+      name : 'Sandra Adams',
+      imageUrl : "https://pickaface.net/gallery/avatar/29659976_190208_1931_x7bs7.png",
+      last_message: "Do you have Paris recommendations"
+    },
+    {
+      name : 'Scott Styris',
+      imageUrl : "https://pickaface.net/gallery/avatar/unr_fake_190524_1549_9fgji7.png",
+      last_message: "Wish I could come, but I'm out of town this"
+    }
+  ]
+
+  const senderList = senders ? senders.map(sender => <SenderListItem sender = {sender}/>) : null
+
+  return (
+    <List>
+      {senderList}
+    </List>
+  )
+}
+
+export default SenderList
