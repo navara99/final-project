f32845671b8ceadcaf6af52a1b645eb2f5c0250f
Siu <siuc@protonmail.com>
2022-01-17 13:25:02 -0500

    clean the files for new components

--- client/src/components/Fair/index.jsx
+++ client/src/components/Fair/index.jsx
@@ -1,32 +1,18 @@
-import React, { useEffect, useState } from "react";
-import Grid from "@mui/material/Grid";
-import CompanyList from "./CompanyList";
-import FairHeader from "./FairHeader";
+import React from "react";
 import { useParams } from "react-router-dom";
 import useFairDetails from "../../hooks/useFairDetails";
 
 const Fair = () => {
   let { id } = useParams();
-  const { organizations, fair_name, fair_desc, host_name, poster } =
-    useFairDetails(id);
+  const { fair, organizations } = useFairDetails(id);
+
   return (
-    <Grid container spacing={2}>
-      {fair_name && (
+    <>
+      {fair && (
         <>
-          <Grid item xs={12} mx={2}>
-            <FairHeader
-              fair_name={fair_name}
-              fair_desc={fair_desc}
-              host_name={host_name}
-              poster={poster}
-            />
-          </Grid>
-          <Grid item xs={12} mt={2}>
-            <CompanyList organizations={organizations} />
-          </Grid>
         </>
       )}
-    </Grid>
+    </>
   );
 };
 
--- client/src/hooks/useFairDetails.js
+++ client/src/hooks/useFairDetails.js
@@ -2,24 +2,17 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useFairDetails = (id) => {
-  const [organizations, setOrganizations] = useState([]);
-  const [fair_name, setFairName] = useState("");
-  const [fair_desc, setFairDesc] = useState("");
-  const [host_name, setHostName] = useState("");
-  const [poster, setPoster] = useState("");
+  const [organizations, setOrganizations] = useState();
+  const [fair, setFair] = useState();
   useEffect(() => {
     axios.get(`/api/fairs/${id}`).then(({ data }) => {
-      const { organizations, fair_name, fair_desc, host_name, poster } = data;
-      console.log(data);
+      const { organizations, fair } = data;
+      setFair(fair);
       setOrganizations(organizations);
-      setFairName(fair_name);
-      setFairDesc(fair_desc);
-      setHostName(host_name);
-      setPoster(poster);
     });
   }, []);
 
-  return { organizations, fair_name, fair_desc, host_name, poster };
+  return { organizations, fair };
 };
 
 export default useFairDetails;
