ec9a83c61888570cdba013283061ec89e875da19
Siu <siuc@protonmail.com>
2022-01-16 23:09:44 -0500

    get job fair (as organization)

--- server/db/queryHelpers/schedule.js
+++ server/db/queryHelpers/schedule.js
@@ -11,8 +11,9 @@ const makeConductInterviewData = (data) => {
       job_title,
       candidate_id,
       application_id,
+      organization_name
     } = event;
-    const title = `Interview ${candidate_first_name} ${candidate_last_name} for ${job_title}`;
+    const title = `Interview ${candidate_first_name} ${candidate_last_name} for ${job_title} (${organization_name})`;
     return {
       start,
       end,
@@ -44,6 +45,27 @@ const makeInterviewData = (data) => {
   });
 };
 
+const makeStallData = (data) => {
+  return getData(data).map((event) => {
+    const {
+      start,
+      end,
+      fair_name,
+      organization_name,
+      fair_id
+    } = event;
+    const title = `${fair_name} (${organization_name})`;
+    return {
+      start,
+      end,
+      title,
+      fairId: fair_id,
+      isInterview: false,
+      asJobSeeker: false,
+    };
+  });
+}
+
 const queryGenerator = (db) => {
   const getSchedule = async (id) => {
     const values = [id];
@@ -55,11 +77,13 @@ const queryGenerator = (db) => {
       users.last_name AS candidate_last_name,
       jobs.name AS job_title,
       users.id AS candidate_id,
-      applications.id AS application_id
+      applications.id AS application_id,
+      organizations.name AS organization_name
       FROM interviews
       JOIN applications ON interviews.application_id = applications.id
       JOIN users ON users.id = applications.user_id
       JOIN jobs ON applications.job_id = jobs.id
+      JOIN organizations ON jobs.employer_id = organizations.id
       WHERE interviewer_id = $1;
     `;
 
@@ -76,18 +100,16 @@ const queryGenerator = (db) => {
     `;
 
     const stallQueryString = `
-      SELECT interviews.start_time AS start,
-      interviews.end_time AS end,
-      users.first_name AS candidate_first_name,
-      users.last_name AS candidate_last_name,
-      jobs.name AS job_title,
-      users.id AS candidate_id,
-      applications.id AS application_id
-      FROM interviews
-      JOIN applications ON interviews.application_id = applications.id
-      JOIN users ON users.id = applications.user_id
-      JOIN jobs ON applications.job_id = jobs.id
-      WHERE interviewer_id = $1;
+      SELECT fairs.name AS fair_name,
+      fairs.start_time AS start,
+      fairs.end_time AS end,
+      fair_id,
+      organizations.name AS organization_name
+      FROM fairs
+      JOIN fairs_organizations ON fair_id = fairs.id
+      JOIN organizations ON fairs_organizations.organization_id = organizations.id
+      JOIN users_organizations ON users_organizations.organization_id = organizations.id
+      WHERE user_id = $1;;
     `;
 
     try {
@@ -95,11 +117,12 @@ const queryGenerator = (db) => {
         interviewerQueryString,
         values
       );
-
       const dataInterview = await db.query(intervieweeQueryString, values);
+      const dataStall = await db.query(stallQueryString, values);
 
       let events = makeConductInterviewData(dataConductInterview);
       events = events.concat(makeInterviewData(dataInterview));
+      events = events.concat(makeStallData(dataStall));
 
       console.log("EVENTS", events);
       return events;
