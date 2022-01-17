d31143c87d6cb6e6f49772dcd1c34460aeb6dc02
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 13:26:16 -0500

    Add functionality to Selecting receiverId

--- client/src/components/ChatBox/Sender/SenderList.jsx
+++ client/src/components/ChatBox/Sender/SenderList.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import SenderListItem from './SenderListItem'
 import { List } from '@mui/material'
-const SenderList = ({senders}) => {
+const SenderList = ({senders, setReceiverId, handleOnClick}) => {
 
-  const senderList = senders ? senders.map(sender => <SenderListItem key={sender.id}sender = {sender}/>) : null
+  const senderList = senders ? senders.map(sender => <SenderListItem key={sender.id} sender = {sender} setReceiverId = {setReceiverId} handleOnClick = {handleOnClick}/>) : null
 
   return (
     <List>
--- client/src/components/ChatBox/Sender/SenderListItem.jsx
+++ client/src/components/ChatBox/Sender/SenderListItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { List, ListItem, ListItemAvatar, Avatar,ListItemText, Typography, Divider, ListItemButton} from '@mui/material'
 
-const SenderListItem = ({sender}) => {
-  const {first_name, last_name, profile_picture, username} = sender;
+const SenderListItem = ({sender, setReceiverId}) => {
+  const {first_name, last_name, profile_picture, username, id} = sender;
   return (  
     <>
-      <ListItemButton alignItems="flex-start">
+      <ListItemButton alignItems="flex-start" onClick={(e) => setReceiverId(id)}>
           <ListItemAvatar>
           <Avatar alt={first_name} src={profile_picture} />
           </ListItemAvatar>
