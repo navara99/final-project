c895a0ce84e8bfed5f7846939a2124bcfd8271bd
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 09:43:56 -0500

    Send contacts with messages array

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -8,8 +8,8 @@ module.exports = (db) => {
   router.get("/", async(req,res) => {
     try {
       const { user_id } = req.session;
-      const messages = await getMessagesByUserId(user_id);
-      res.status(200).json(messages);
+      const {messagesArr, contacts} = await getMessagesByUserId(user_id);
+      res.status(200).json({messagesArr,contacts});
     } catch (error) {
       console.log(error);
     }
