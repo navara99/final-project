2a72fb4fda4df2fb6fbec264ca3631625f1f13b2
Siu <siuc@protonmail.com>
2022-01-17 12:27:44 -0500

    format

--- client/src/hooks/useFairDetails.js
+++ client/src/hooks/useFairDetails.js
@@ -3,23 +3,22 @@ import axios from "axios";
 
 const useFairDetails = (id) => {
   const [organizations, setOrganizations] = useState([]);
-  const [fair_name, setFairName] = useState('');
-  const [fair_desc, setFairDesc] = useState('');
-  const [host_name, setHostName] = useState('');
-  const [poster, setPoster] = useState('');
+  const [fair_name, setFairName] = useState("");
+  const [fair_desc, setFairDesc] = useState("");
+  const [host_name, setHostName] = useState("");
+  const [poster, setPoster] = useState("");
   useEffect(() => {
-    axios.get(`/api/fairs/${id}`).then(({data}) => {
-      const {organizations, fair_name, fair_desc, host_name,poster} = data;
+    axios.get(`/api/fairs/${id}`).then(({ data }) => {
+      const { organizations, fair_name, fair_desc, host_name, poster } = data;
       setOrganizations(organizations);
       setFairName(fair_name);
       setFairDesc(fair_desc);
       setHostName(host_name);
-      setPoster(poster); 
+      setPoster(poster);
     });
   }, []);
 
-  return {organizations, fair_name, fair_desc, host_name, poster}
-
-}
+  return { organizations, fair_name, fair_desc, host_name, poster };
+};
 
 export default useFairDetails;
