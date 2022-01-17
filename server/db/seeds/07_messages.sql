552a4874cb31d98ce8d1ac2277bab3385121d10e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 14:58:08 -0500

    Add raw message seeds

--- server/db/seeds/07_messages.sql
+++ server/db/seeds/07_messages.sql
@@ -3,5 +3,11 @@ VALUES
   (1, 2, 'Hello'),
   (2, 1, 'Hi'),
   (1, 2, 'How are you doing?'),
-  (2, 1, 'I am good. Thanks!')
+  (2, 1, 'I am good. Thanks!'),
+  (3,2, 'Need Help?'),
+  (2,3, 'What is it about'),
+  (3,2, 'Socket io'),
+  (2,3, 'Ohh'),
+  (1,3, 'You need help?'),
+  (3,1, 'Yeah!')
 ;
