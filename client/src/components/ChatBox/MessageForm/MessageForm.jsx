e3c09b6341cc9f70ce266d316fb737749c9d439a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 01:36:41 -0500

    Add message form for sending message

--- client/src/components/ChatBox/MessageForm/MessageForm.jsx
+++ client/src/components/ChatBox/MessageForm/MessageForm.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, TextField,Fab } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send';
 const MessageForm = () => {
+  const [message, setMessage] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("mes", message);
+
+    setMessage('');
+  }
   return (
-    <Grid container sx={{p: '20px'}}>
+    <Grid container sx={{p: '20px'}} component="form" onSubmit={handleSubmit}>
         <Grid item xs={11}>
-            <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+            <TextField id="message" name="message"  required fullWidth value={message} onChange={(e) => setMessage(e.target.value)}/>
         </Grid>
         <Grid item xs={1} align="right">
-            <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+            <Fab color="primary" aria-label="add" type="submit"><SendIcon /></Fab>
         </Grid>
     </Grid>
   )
