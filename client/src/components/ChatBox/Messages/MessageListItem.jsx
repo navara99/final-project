828f91d6f7b82c8a27188b4d45ea3055ed1e5c2a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 01:37:57 -0500

    pass currentUser as props

--- client/src/components/ChatBox/Messages/MessageListItem.jsx
+++ client/src/components/ChatBox/Messages/MessageListItem.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import {ListItem,Grid,ListItemText,Paper } from "@mui/material";
 import moment from "moment"
 
+
 const MessageListItem = (props) => {
-  const {message, created_at, sender_id} = props.message
+  const {message, created_at, sender_id} = props.message;
+  const currentUser = props.currentUser;
   return (
+    <>
+   {currentUser && 
     <ListItem >
       <Grid container>
           <Grid item xs={12} >
-              <ListItemText align={sender_id === 1 ? "right" : "left"} primary={message}></ListItemText>
+              <ListItemText align={sender_id === currentUser.id? "right" : "left"} primary={message}></ListItemText>
           </Grid>
           <Grid item xs={12}>
-              <ListItemText align={sender_id === 1 ? "right" : "left"} secondary={moment(`${created_at}`).fromNow()}></ListItemText>
+              <ListItemText align={sender_id === currentUser.id ? "right" : "left"} secondary={moment(`${created_at}`).fromNow()}></ListItemText>
           </Grid>
       </Grid>
     </ListItem> 
+    }
+    </>
   )
 }
 
