624c9199be7c0baa11a36cd1f5d939a30479fce6
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-17 02:43:06 -0500

    Add post route for message

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   const queryGenerator = require("../db/queryHelpers");
-  const { getMessagesByUserId } = queryGenerator(db);
+  const { getMessagesByUserId, createNewMessage } = queryGenerator(db);
   //Get All Messages for users
   router.get("/", async(req,res) => {
     try {
@@ -13,8 +13,16 @@ module.exports = (db) => {
     } catch (error) {
       console.log(error);
     }
-    
   });
+
+  router.post("/", async(req,res) => {
+    try {
+      const newMessage =  await createNewMessage(req.body);
+      res.status(200).json(newMessage);
+    } catch (error) {
+      console.log(error);
+    }
+  })
 
   return router;
 }
