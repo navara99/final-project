c77cad257f9763a6521e271eac69024ab700bea1
Siu <siuc@protonmail.com>
2022-01-17 17:53:06 -0500

    Revert "add new function to fetch data"
    This reverts commit 93b0ff97801b197403df4e24b5093e1070f7e4a2.

--- client/src/hooks/useFairDetails.js
+++ client/src/hooks/useFairDetails.js
@@ -4,24 +4,17 @@ import axios from "axios";
 const useFairDetails = (id) => {
   const [stalls, setStalls] = useState();
   const [fair, setFair] = useState();
-  const [added, setAdded] = useState();
-
-  const updateFairDetails = () => {
-    axios.get(`/api/fairs/${id}`).then(({ data }) => {
-      const { stalls, fair, added } = data;
-      setFair(fair);
-      setStalls(stalls);
-      setAdded(added);
-    });
-  };
-
   useEffect(() => {
     axios.get(`/api/fairs/${id}`).then(({ data }) => {
-      updateFairDetails();
+      const { stalls, fair } = data;
+      setFair(fair);
+      setStalls(stalls);
+      console.log(fair)
+      console.log(stalls)
     });
   }, []);
 
-  return { stalls, fair, added, updateFairDetails };
+  return { stalls, fair };
 };
 
 export default useFairDetails;
