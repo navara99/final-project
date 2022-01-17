d1de30574be8be454e1bda0ea09aad2ba9897933
Siu <siuc@protonmail.com>
2022-01-16 22:42:31 -0500

    refactoring

--- server/db/queryHelpers/schedule.js
+++ server/db/queryHelpers/schedule.js
@@ -1,10 +1,48 @@
 const getData = ({ rows }) => rows;
 const getFirstRecord = (result) => getData(result)[0];
 
+const makeConductInterviewData = (data) => {
+  return getData(data).map((event) => {
+    const {
+      start,
+      end,
+      candidate_first_name,
+      candidate_last_name,
+      job_title,
+      candidate_id,
+      application_id,
+    } = event;
+    const title = `Interview ${candidate_first_name} ${candidate_last_name} for ${job_title}`;
+    return {
+      start,
+      end,
+      title,
+      candidateId: candidate_id,
+      applicationId: application_id,
+      isInterview: true,
+    };
+  });
+};
+
 const queryGenerator = (db) => {
   const getSchedule = async (id) => {
     const values = [id];
     const conductInterviewQueryString = `
+    SELECT interviews.start_time AS start,
+    interviews.end_time AS end,
+    users.first_name AS candidate_first_name,
+    users.last_name AS candidate_last_name,
+    jobs.name AS job_title,
+    users.id AS candidate_id,
+    applications.id AS application_id
+    FROM interviews
+    JOIN applications ON interviews.application_id = applications.id
+    JOIN users ON users.id = applications.user_id
+    JOIN jobs ON applications.job_id = jobs.id
+    WHERE interviewer_id = $1;
+  `;
+
+  const interviewQueryString = `
     SELECT interviews.start_time AS start,
     interviews.end_time AS end,
     users.first_name AS candidate_first_name,
@@ -24,25 +62,7 @@ const queryGenerator = (db) => {
         conductInterviewQueryString,
         values
       );
-      const events = getData(dataConductInterview).map((event) => {
-        const {
-          start,
-          end,
-          candidate_first_name,
-          candidate_last_name,
-          job_title,
-          candidate_id,
-          application_id,
-        } = event;
-        const title = `Interview ${candidate_first_name} ${candidate_last_name} for ${job_title}`;
-        return {
-          start,
-          end,
-          title,
-          candidateId: candidate_id,
-          applicationId: application_id,
-        };
-      });
+      let events = makeConductInterviewData(dataConductInterview);
       console.log("EVENTS", events);
       return events;
     } catch (err) {
