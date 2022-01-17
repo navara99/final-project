5ed1fc1ecad60a8e05340aefc04cfe852a4f2d6e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-16 19:55:03 -0500

    format time for messages

--- client/src/components/ChatBox/Messages/MessageListItem.jsx
+++ client/src/components/ChatBox/Messages/MessageListItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import {ListItem,Grid,ListItemText,Paper } from "@mui/material"
-const MessageListItem = ({message}) => {
-  const {text, created_at, sender_id} = message
+import {ListItem,Grid,ListItemText,Paper } from "@mui/material";
+import moment from "moment"
+
+const MessageListItem = (props) => {
+  const {message, created_at, sender_id} = props.message
   return (
     <ListItem >
       <Grid container>
           <Grid item xs={12} >
-              <ListItemText align={sender_id === 1 ? "right" : "left"} primary={text}></ListItemText>
+              <ListItemText align={sender_id === 1 ? "right" : "left"} primary={message}></ListItemText>
           </Grid>
           <Grid item xs={12}>
-              <ListItemText align={sender_id === 1 ? "right" : "left"} secondary={created_at}></ListItemText>
+              <ListItemText align={sender_id === 1 ? "right" : "left"} secondary={moment(`${created_at}`).fromNow()}></ListItemText>
           </Grid>
       </Grid>
     </ListItem> 
