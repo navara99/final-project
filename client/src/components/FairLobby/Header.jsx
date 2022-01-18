d37f9e0e2ae97b19fb61e2db6730689eff67a6d1
Siu <siuc@protonmail.com>
2022-01-18 18:58:45 -0500

    remove unused components

--- client/src/components/FairLobby/Header.jsx
+++ client/src/components/FairLobby/Header.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
-import ScheduleButton from "./ScheduleButton";
-import JoinButton from "./JoinButton";
+import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
 import { formatStartEndTime, formatDate } from "../../helpers/date";
 
 const Header = ({ id, fair, added, currentUser, add, updateFairDetails }) => {
@@ -26,20 +23,28 @@ const Header = ({ id, fair, added, currentUser, add, updateFairDetails }) => {
   return (
     <>
       {fair && (
-        <Card>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Date: {formatDate(start_time)}
-              <br />
-              Time: {formatStartEndTime(start_time, end_time)}
-              <br />
-              {description}
-            </Typography>
-          </CardContent>
-        </Card>
+        <Box>
+          <Card variant="outlined">
+            <CardMedia
+              component="img"
+              height="150"
+              image={poster}
+              alt="fair poster"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Date: {formatDate(start_time)}
+                <br />
+                Time: {formatStartEndTime(start_time, end_time)}
+                <br />
+                {description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
       )}
     </>
   );
