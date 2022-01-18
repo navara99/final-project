ea6079d8467e3e3389020972c219c41503d74089
Tharsikan <thar.navaratnam@gmail.com>
2022-01-18 23:35:54 +0000

    add message column

--- server/db/schema/09_applications.sql
+++ server/db/schema/09_applications.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS applications CASCADE;
+DROP TABLE IF EXISTS applications
+CASCADE;
 
-CREATE TABLE applications (
+CREATE TABLE applications
+(
   id SERIAL PRIMARY KEY,
+  message varchar,
   user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   job_id int NOT NULL REFERENCES jobs(id) ON DELETE CASCADE,
   resume varchar NOT NULL
