6e39112b9c70dbf47a57534035ef789d757182f8
Tharsikan <thar.navaratnam@gmail.com>
2022-01-18 23:29:06 +0000

    successfuly recieve resume and contents from form

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, "./public/uploads/");
+    console.log("raeched")
+    cb(null, "./public/uploads");
   },
   filename: function (req, file, cb) {
-    console.log(Date.now());
     cb(null, Date.now() + '_' + file.originalname);
   }
 });
-const upload = multer({ storage });
-
+const upload = multer({ storage: storage, limits: { fieldSize: 10 * 1024 * 1024 } });
 
 module.exports = (db) => {
   const queryGenerator = require("../db/queryHelpers");
@@ -21,8 +20,9 @@ module.exports = (db) => {
     const { user_id } = req.session;
     const { jobId, message } = req.body;
     console.log(jobId, message);
+    console.log(req.body);
     try {
-      console.log(req.file);
+      console.log(req.file.path);
       res.json({
         status: "success"
       });
