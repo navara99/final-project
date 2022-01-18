ef8e0ce880423d35d6666d857764fe9dab59d5bd
Tharsikan <thar.navaratnam@gmail.com>
2022-01-18 23:41:56 +0000

    update seeds with new message column

--- server/db/seeds/09_applications.sql
+++ server/db/seeds/09_applications.sql
@@ -1,38 +1,10 @@
 INSERT INTO applications (
   user_id,
   job_id,
-  resume
+  resume,
+  message
 )
 VALUES
-(4, 1, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
-
-Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
-
-Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
-Chief Design Engineer'),
-(2, 2, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
-
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
-
-Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
-
-Fusce consequat. Nulla nisl. Nunc nisl.
-
-Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
-
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.'),
-(1, 2, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
-
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
-
-Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
-
-Fusce consequat. Nulla nisl. Nunc nisl.
-
-Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
-
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.');
+(4, 1, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. '),
+(2, 2, 'Curabitur gravida nisi at nibh.','Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. '),
+(1, 2, 'Curabitur gravida Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.','Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. ')
