db29959d14b88cd5aeffc7e73a378fcf6bd461a8
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-18 20:00:34 -0500

    Add new Component for setting page

---
+++ client/src/components/ProfileSetting/EditPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditPassword = () => {
+  return (
+    <div>
+      <h1>Edit password form</h1>
+    </div>
+  )
+}
+
+export default EditPassword
---
+++ client/src/components/ProfileSetting/ProfileSetting.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import EditProfile from './EditProfile'
+import NavColumn from './NavColumn'
+
+const ProfileSetting = () => {
+  return (
+    <div>
+      <NavColumn/>
+      <EditProfile/>
+      <EditProfile/>
+    </div>
+  )
+}
+
+export default ProfileSetting
