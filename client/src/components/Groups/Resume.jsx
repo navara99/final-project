7a8db6e4e027e7c1e748295b46e33eb675eb28e1
Tharsikan <thar.navaratnam@gmail.com>
2022-01-19 14:08:41 -0500

    finish resume modal

--- client/src/components/Groups/Resume.jsx
+++ client/src/components/Groups/Resume.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(theme => ({
+  dialogPaper: {
+    height: "90%"
+  }
+}));
 
 function Resume({ application, openResume, setOpenResume }) {
-
+  const classes = useStyles();
 
   return (
     <>
-      <Dialog open={openResume} onClose={() => { }}>
+      <Dialog open={openResume} fullWidth={true} maxWidth={"lg"} onClose={() => { }} classes={{ paper: classes.dialogPaper }}>
         <DialogTitle>{application.first_name}'s Resume</DialogTitle>
         <DialogContent>
-          <embed src={"http://localhost:8080/" + application.resume + "#toolbar=0"} width="100%" height="80%" />
+          <embed src={"http://localhost:8080/" + application.resume + "#toolbar=0"} height="100%" width="100%" />
         </DialogContent>
         <DialogActions>
           <Button onClick={() => { setOpenResume(!openResume) }}>Cancel</Button>
