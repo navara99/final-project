59e5f27574385d783b150cc30473c127769200f4
Tharsikan <thar.navaratnam@gmail.com>
2022-01-19 01:25:58 +0000

    add test resume file path to seeds

--- server/db/seeds/09_applications.sql
+++ server/db/seeds/09_applications.sql
@@ -1,10 +1,11 @@
-INSERT INTO applications (
+INSERT INTO applications
+  (
   user_id,
   job_id,
-  resume,
-  message
-)
+  message,
+  resume
+  )
 VALUES
-(4, 1, 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. '),
-(2, 2, 'Curabitur gravida nisi at nibh.','Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. '),
-(1, 2, 'Curabitur gravida Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.','Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. ')
+  (4, 1, 'Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. ', 'public/uploads/test.pdf'),
+  (2, 2, 'Gingerbread cheesecake gummies cheesecake brownie sugar plum macaroon. Biscuit apple pie macaroon shortbread tiramisu cookie. Sweet roll shortbread fruitcake croissant pastry cupcake lollipop. ', 'public/uploads/test.pdf'),
+  (1, 2, 'Curabitur gravida Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'public/uploads/test.pdf')
