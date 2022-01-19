712e5de366c5f7c4a41f0f87b4a07e56837f36a1
Siu <siuc@protonmail.com>
2022-01-19 18:51:34 -0500

    Update Controls.jsx

---
+++ client/src/components/Stall/Controls.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useClient } from "./setting";
+import { Add, Button, Icon } from "@mui/icons-material";
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import VideocamOffIcon from '@mui/icons-material/VideocamOff';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
