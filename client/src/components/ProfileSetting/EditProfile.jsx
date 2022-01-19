c7b9bf966a352450d37594db0ae85f2f862a677b
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-19 11:36:51 -0500

    Add value to  inputs with usestate

--- client/src/components/ProfileSetting/EditProfile.jsx
+++ client/src/components/ProfileSetting/EditProfile.jsx
@@ -1,9 +1,28 @@
 import { Grid, TextField, Typography,InputLabel, Avatar, Divider, Button } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const EditProfile = ({currentUser}) => {
- 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  const [bio, setBio] = useState("");
+
+  useEffect(() => {
+    if(currentUser) {
+      const {first_name, last_name, email, username, profile_picture, bio} = currentUser;
+      setFirstName(first_name);
+      setLastName(last_name);
+      setEmail(email);
+      setUsername(username);
+      setProfilePicture(profile_picture);
+      setBio(bio);
+    }
+
+  },[currentUser])
+
   return (
     <>
     {
@@ -14,16 +33,16 @@ const EditProfile = ({currentUser}) => {
               </Grid>
               <Grid item container component="form" direction="column" sx={{flexGrow: 1}} justifyContent="space-evenly">
                 <Box sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}> 
-                    <TextField id="firstName" label="First name" variant="outlined" sx={{flexGrow:1,mr:2, bgcolor:"white"}}  value={currentUser.first_name} required/>
-                    <TextField id="lastName" label="Last name" variant="outlined" sx={{flexGrow:1,bgcolor:"white"}} value={ currentUser.last_name} required/>
+                    <TextField id="firstName" label="First name" variant="outlined" sx={{flexGrow:1,mr:2, bgcolor:"white"}}  value={firstName} required/>
+                    <TextField id="lastName" label="Last name" variant="outlined" sx={{flexGrow:1,bgcolor:"white"}} value={ lastName} required/>
                 </Box>
-                <TextField id="email" label="Email" variant="outlined" sx={{bgcolor:"white"}} value={ currentUser.email} required/>
-                <TextField id="userName" label="User name" variant="outlined" sx={{bgcolor:"white"}} value={ currentUser.username} required/>
+                <TextField id="email" label="Email" variant="outlined" sx={{bgcolor:"white"}} value={email} required/>
+                <TextField id="userName" label="User name" variant="outlined" sx={{bgcolor:"white"}} value={username} required/>
                 <Box sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                  <Avatar alt={ currentUser.username} src={ currentUser.profile_picture}/>
-                  <TextField sx={{flexGrow:1, ml:2, bgcolor:"white"}}id="profile_picture" label="Picture URL" variant="outlined" value={currentUser.profile_picture} required />
+                  <Avatar alt={ username} src={profilePicture}/>
+                  <TextField sx={{flexGrow:1, ml:2, bgcolor:"white"}}id="profile_picture" label="Picture URL" variant="outlined" value={profilePicture} required />
                 </Box>
-                <TextField id="bio" multiline label="Bio"variant="outlined" sx={{bgcolor:"white"}} value={ currentUser.bio}/>
+                <TextField id="bio" multiline label="Bio"variant="outlined" sx={{bgcolor:"white"}} value={bio}/>
                 <Divider/>
                 <Grid item>
                   <Button variant='contained'size='large'>Save</Button>
