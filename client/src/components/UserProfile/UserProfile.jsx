b766e3e05bca5ebbc756299c91828d01ab426adc
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-20 04:01:55 -0500

    Add design to profile picture

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,26 +1,43 @@
-import { Avatar, Grid, Typography } from '@mui/material';
+import { Avatar, Grid, Stack, Typography, Box, Badge, Divider, ListItemText , List} from '@mui/material';
+// import { styled } from '@mui/system';
+import { styled } from '@mui/material/styles';
 import React from 'react';
+
+const ProfilePic = styled(Avatar)(({ theme }) => ({
+  width:150, 
+  height:150,
+  border: `2px solid ${theme.palette.background.paper}`,
+
+}));
+
 const UserProfile = ({currentUser}) => {
   return ( <>{
     currentUser &&
-    (<Grid xs={12} container sx={{backgroundColor:"#eff2f6"}}>
-    <Grid item container direction="column">
-      <Grid>
-        <Avatar alt="profile" src={currentUser.profile_picture} sx={{width:150, height:150}}/>
-        <Typography>{`${currentUser.first_name} ${currentUser.last_name}`}</Typography>
-        <Typography>{currentUser.usename}</Typography>
-        <Typography>{currentUser.email}</Typography>
-        <Typography>{currentUser.bio}</Typography>
-      </Grid>
-      <Grid>
-        <Typography>List of Organizations</Typography>
-      </Grid>
-      <Grid>
-        <Typography>Applied Jobs</Typography>
-      </Grid>
-      
-    </Grid>
-  </Grid>)
+    (
+    <Grid xs={12} container sx={{backgroundColor:"#eff2f6"}} justifyContent="space-around">
+      <Stack spacing={3} >
+          <Box sx={{bgcolor:'#1f78b1'}}>
+            <Badge
+            sx={{bgcolor:'#1f78b1', width:700}}
+              overlap="circular"
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              badgeContent={
+                <ProfilePic alt={currentUser.first_name} src={currentUser.profile_picture} />
+              }
+            >
+            <Box component="span" sx={{height:150, width:500}}/>
+            </Badge>
+          </Box>
+          <Box sx={{pt:6,px:3, display:'flex', alignItems:"end"}}>
+            <Typography variant='h4' sx={{}}>{`${currentUser.first_name} ${currentUser.last_name}`}</Typography>
+            <Typography variant='body1' sx={{alignSelf:"end", pl:1}}>{`( ${currentUser.username} )`}</Typography>
+          </Box>
+          <Box sx={{px:3}}>
+            <Typography variant='h6'>About</Typography>
+            <Typography variant='body1' sx={{pt:1}}>{currentUser.bio}</Typography>
+          </Box>
+      </Stack>
+    </Grid>)
   }
 
   </>
