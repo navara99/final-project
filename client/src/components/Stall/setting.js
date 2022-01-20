e6439838e7c49990e7327ed3fd568ef1c7f57a24
Siu <siuc@protonmail.com>
2022-01-20 12:09:59 -0500

    update token

--- client/src/components/Stall/setting.js
+++ client/src/components/Stall/setting.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "7fbb0d7a140c49ddb9a80357e303fb88";
 const token =
-  "0067fbb0d7a140c49ddb9a80357e303fb88IAChfSEzziXwal7i+7/nOtdI8/72i0RjIdTkhYAnV9+CnAx+f9gAAAAAEADC8VeAoyvqYQEAAQCjK+ph";
+  "0067fbb0d7a140c49ddb9a80357e303fb88IADzfdTTKHiOtSDB1lk74zgZsIRa/XjDr2IJqyFGjuiJ9Ax+f9gAAAAAEADC8VeAX+bqYQEAAQBe5uph";
 export const config = { mode: "rtc", codec: "vp8", appId, token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
