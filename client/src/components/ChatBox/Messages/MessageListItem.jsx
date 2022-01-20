774f21066d2f052296ef809f5adce68d404e431f
Siu <siuc@protonmail.com>
2022-01-20 17:39:26 -0500

    Update MessageListItem.jsx

--- client/src/components/ChatBox/Messages/MessageListItem.jsx
+++ client/src/components/ChatBox/Messages/MessageListItem.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import {ListItem,Grid,ListItemText} from "@mui/material";
-import moment from "moment"
-import BasicScrollToBottom from 'react-scroll-to-bottom/lib/BasicScrollToBottom';
-
+import React from "react";
+import { ListItem, Grid, ListItemText } from "@mui/material";
+import moment from "moment";
+import BasicScrollToBottom from "react-scroll-to-bottom/lib/BasicScrollToBottom";
 
 const MessageListItem = (props) => {
-  const {message, created_at, sender_id} = props.message;
+  const { message, created_at, sender_id } = props.message;
   const currentUser = props.currentUser;
 
   return (
     <>
-   {currentUser && 
-    <ListItem >
-      <Grid container>
-          <Grid item xs={12} >
-              <ListItemText align={sender_id === currentUser.id? "right" : "left"} primary={message} ></ListItemText>
+      {currentUser && (
+        <ListItem>
+          <Grid container>
+            <Grid item xs={12}>
+              <ListItemText
+                align={sender_id === currentUser.id ? "right" : "left"}
+                primary={message}
+              ></ListItemText>
+            </Grid>
+            <Grid item xs={12}>
+              <ListItemText
+                align={sender_id === currentUser.id ? "right" : "left"}
+                secondary={moment(`${created_at}`).fromNow()}
+              ></ListItemText>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-              <ListItemText align={sender_id === currentUser.id ? "right" : "left"} secondary={moment(`${created_at}`).fromNow()}></ListItemText>
-          </Grid>
-      </Grid>
-    </ListItem> 
-   
-    }
+        </ListItem>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default MessageListItem
+export default MessageListItem;
