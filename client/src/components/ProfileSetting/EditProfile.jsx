6a1f5afbab8f53d32a39e4f809aeb238a917973e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-20 15:36:20 -0500

    Sending resume file to server while updating user profile

--- client/src/components/ProfileSetting/EditProfile.jsx
+++ client/src/components/ProfileSetting/EditProfile.jsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/system'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 
 const EditProfile = ({setCurrentUser, currentUser, setErrorMessage, setShowError}) => {
@@ -15,6 +16,7 @@ const EditProfile = ({setCurrentUser, currentUser, setErrorMessage, setShowError
     profilePicture:"",
     bio:""
   });
+  const [resume, setResume] = useState();
   useEffect(() => {
     if(currentUser) {
       const {first_name, last_name, email, username, profile_picture, bio} = currentUser;
@@ -39,7 +41,16 @@ const EditProfile = ({setCurrentUser, currentUser, setErrorMessage, setShowError
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("/api/users/edit", userInfo).then(res =>{
+    const updateUserInfo = new FormData();
+    updateUserInfo.append("firstName", userInfo.firstName);
+    updateUserInfo.append("lastName", userInfo.lastName);
+    updateUserInfo.append("email", userInfo.email);
+    updateUserInfo.append("username", userInfo.username);
+    updateUserInfo.append("profilePicture", userInfo.profilePicture);
+    updateUserInfo.append("bio",userInfo.bio )
+    updateUserInfo.append("resume", resume);
+    console.log("resume",resume)
+    axios.post("/api/users/edit", updateUserInfo).then(res =>{
       setCurrentUser(res.data)
       navigate("/profile")
     }).catch(error => {
@@ -66,20 +77,25 @@ const EditProfile = ({setCurrentUser, currentUser, setErrorMessage, setShowError
                   <Avatar alt={userInfo.username} src={userInfo.profilePicture}/>
                   <TextField sx={{flexGrow:1, ml:2, bgcolor:"white"}}id="profile_picture" name="profilePicture" label="Picture URL" variant="outlined" value={userInfo.profilePicture} onChange={handleChange} />
                 </Box>
-                <Button
-                  variant="contained"
-                  component="label"
-                  sx={{width:'20%', mt:2}}
-                >Upload Resume<input
-                    type="file"
-                   
-                    name="resume"
-                    hidden />
-                </Button>
+                <Box>
+                  <Button
+                    variant="contained"
+                    component="label"
+                    sx={{width:'20%', mt:2}}
+                    startIcon={<FileUploadIcon/>}
+                  >Upload Resume<input
+                      type="file"
+                      onChange={(e) => setResume(e.target.files[0])}        
+                      name="resume"
+                      hidden />
+                  </Button>
+                  <span style={{paddingLeft: 10}}>{resume && resume.name}</span>
+                </Box>
+               
                 <TextField id="bio" name="bio" multiline label="Bio"variant="outlined" sx={{bgcolor:"white", mt:2}} value={userInfo.bio} onChange={handleChange}/>
                 <Divider/>
                 <Grid item>
-                  <Button variant='contained'size='large' type="submit">Save</Button>
+                  <Button variant='contained'size='large' type="submit" color="success">Save</Button>
                 </Grid>
               </Grid>
             </Grid>
