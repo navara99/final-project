588f9c15cc7f10ba2b724ece43f2cec24078a17c
Siu <siuc@protonmail.com>
2022-01-21 00:17:33 -0500

    show sender list according to the last message

--- client/src/components/ChatBox/Sender/SenderList.jsx
+++ client/src/components/ChatBox/Sender/SenderList.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import SenderListItem from './SenderListItem'
-import { List } from '@mui/material'
-const SenderList = ({senders, setReceiverId, handleOnClick, setReceiver}) => {
+import React from "react";
+import SenderListItem from "./SenderListItem";
+import { List } from "@mui/material";
+const SenderList = ({
+  messages,
+  senders,
+  setReceiverId,
+  handleOnClick,
+  setReceiver,
+}) => {
+  const senderList = senders
+    ? senders
+        .map((sender) => {
+          const [lastMsg, createdDate, lastUserId, msgId] = messages.reduce(
+            (lastMessage, message) => {
+              if (
+                message.sender_id === sender.id ||
+                message.receiver_id === sender.id
+              ) {
+                return [message.message, message.created_at, message.sender_id, message.id];
+              }
+              return lastMessage;
+            },
+            null
+          );
+          return { ...sender, lastMsg, createdDate, lastUserId, msgId };
+        })
+        .sort((senderA, senderB) => senderB.msgId - senderA.msgId)
+        .map((sender) => {
+          const { lastMsg, createdDate, lastUserId } = sender;
+          return (
+            <SenderListItem
+              key={sender.id}
+              sender={sender}
+              setReceiverId={setReceiverId}
+              handleOnClick={handleOnClick}
+              setReceiver={setReceiver}
+              {...{ lastMsg, createdDate, lastUserId }}
+            />
+          );
+        })
+    : null;
 
-  const senderList = senders ? senders.map(sender => <SenderListItem key={sender.id} sender = {sender} setReceiverId = {setReceiverId} handleOnClick = {handleOnClick} setReceiver = {setReceiver}/>) : null
+  return <List>{senderList}</List>;
+};
 
-  return (
-    <List>
-      {senderList}
-    </List>
-  )
-}
-
-export default SenderList
+export default SenderList;
--- client/src/components/ChatBox/Sender/SenderListItem.jsx
+++ client/src/components/ChatBox/Sender/SenderListItem.jsx
@@ -1,38 +1,53 @@
-import React from 'react'
-import { ListItemAvatar, Avatar,ListItemText, Typography, Divider, ListItemButton} from '@mui/material'
+import React from "react";
+import {
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Typography,
+  Divider,
+  ListItemButton,
+} from "@mui/material";
 
-const SenderListItem = ({sender, setReceiverId, setReceiver}) => {
-  const {first_name, last_name, profile_picture, username, id} = sender;
+const SenderListItem = ({
+  sender,
+  setReceiverId,
+  setReceiver,
+  lastMsg,
+  createdDate,
+  lastUserId,
+}) => {
+  const { first_name, last_name, profile_picture, username, id } = sender;
   const handleClick = (e) => {
     setReceiverId(id);
-    setReceiver({...sender})
-  }
-  return (  
+    setReceiver({ ...sender });
+  };
+
+  console.log(lastMsg, createdDate, lastUserId);
+  return (
     <>
       <ListItemButton alignItems="flex-start" onClick={handleClick}>
-          <ListItemAvatar>
+        <ListItemAvatar>
           <Avatar alt={first_name} src={profile_picture} />
-          </ListItemAvatar>
-          <ListItemText
-          primary={first_name + last_name}
+        </ListItemAvatar>
+        <ListItemText
+          primary={`${first_name} ${last_name}`}
           secondary={
-              <>
+            <>
               <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.secondary"
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.secondary"
               >
                 {username}
               </Typography>
-             
-              </>
+            </>
           }
-          />
+        />
       </ListItemButton>
       <Divider variant="inset" component="li" />
     </>
-    )
-}
+  );
+};
 
-export default SenderListItem
+export default SenderListItem;
