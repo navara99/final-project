c26283280163fa450ca3c6322522d1a6db015b17
Tharsikan <thar.navaratnam@gmail.com>
2022-01-21 21:12:01 +0000

    finish /:id route handling

--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 
 module.exports = (db) => {
-  const queryGenerator = require("../db/queryHelpers");
-  const { } = queryGenerator(db);
+  const interviewQueryGenerator = require("../db/queryHelpers/interview");
+  const { getInterviewById } = interviewQueryGenerator(db);
 
   router.get("/:id", async (req, res) => {
-    
+    const { user_id } = req.session;
+    const { id } = req.params;
 
+    try {
+      const result = await getInterviewById(id);
+      res.json({ ...result, interviewer: user_id === result.interviewer_id })
+    } catch (err) {
+      console.log(err.message);
+    }
 
   });
 
