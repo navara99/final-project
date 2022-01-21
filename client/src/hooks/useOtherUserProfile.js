40e3b7b0fa1c028cfc1413f0816c3e3f5aa1074a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-21 11:29:58 -0500

    change the params name

--- client/src/hooks/useOtherUserProfile.js
+++ client/src/hooks/useOtherUserProfile.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 
-const useOtherUserProfile = () => {
+const useOtherUserProfile = (user_id) => {
   const [otherUser, setOtherUser] = useState();
   useEffect(() => {
-    axios.get(`/api/users/${id}`)
+    axios.get(`/api/users/${user_id}`)
          .then(res => {
            console.log(res.data);
          }).catch(err => {
             console.log(err.response.data.error)
          })
-  }, [id])
+  }, [user_id])
   return [otherUser, setOtherUser];
 }
 
