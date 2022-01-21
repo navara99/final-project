fa7a221418539cda6d73a82f3b4b4a67e3bf4b0b
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-21 11:28:10 -0500

    Change route name with user_id param

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -92,7 +92,7 @@ function App() {
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/messages" element={<ChatBox currentUser = {currentUser}/>} />
           <Route path="/profile" element={<UserProfile currentUser = {currentUser} />} />
-          <Route path="/profile/:id" element={<OtherProfile />} />
+          <Route path="/profile/:user_id" element={<OtherProfile currentUser = {currentUser}/>} />
           <Route path="/settings" element={<ProfileSetting {...{setCurrentUser,currentUser,setErrorMessage,setShowError,setSnackBarDetails}}/>}/>
           <Route path="/jobs/:id/applications" element={<JobApplications />} />
           <Route path="/live/:id" element={<FairLobby currentUser={currentUser} />} />
