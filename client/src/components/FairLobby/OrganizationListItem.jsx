9ae53f5ac77e0741f6de8a1cdaaf58279bfdef19
Siu <siuc@protonmail.com>
2022-01-20 22:53:18 -0500

    remove unused var

--- client/src/components/FairLobby/OrganizationListItem.jsx
+++ client/src/components/FairLobby/OrganizationListItem.jsx
@@ -5,7 +5,6 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
 import CardMedia from "@mui/material/CardMedia";
 
 const OrganizationListItem = ({
