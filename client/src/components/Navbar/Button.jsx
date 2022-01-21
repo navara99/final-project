123636187460ed7c3d13ea2c39244b7f5c73b049
Siu <siuc@protonmail.com>
2022-01-20 23:37:21 -0500

    change color of badge

--- client/src/components/Navbar/Button.jsx
+++ client/src/components/Navbar/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ Icon, to, text, numOfUnreadMsg }) => {
   return (
     <Tippy content={text} placement="right" theme="material" arrow={true}>
       <Link to={to} className="navbar-button">
-        <Badge badgeContent={numOfUnreadMsg} color="primary">
+        <Badge badgeContent={numOfUnreadMsg} color="warning">
           <Icon />
         </Badge>
       </Link>
