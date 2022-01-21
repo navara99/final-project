78e131ebe75e89c6ae6024138cafab5f58fbc9e6
Siu <siuc@protonmail.com>
2022-01-20 22:58:02 -0500

    remove unused var

--- client/src/hooks/useMessages.js
+++ client/src/hooks/useMessages.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { io } from "socket.io-client";
