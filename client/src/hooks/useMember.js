a1d55251ce7a19f287851b13e82e896013c7b8b0
Tharsikan <thar.navaratnam@gmail.com>
2022-01-21 23:58:17 -0500

    add missing dependency arr

--- client/src/hooks/useMember.js
+++ client/src/hooks/useMember.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function useMember() {
+function useMember(id) {
   const [isMember, setIsMember] = useState();
 
   useEffect(() => {
-    axios.get(`api/organizations/${id}/isMember`).then(({ data }) => {
+    axios.get(`/api/organizations/${id}/isMember`).then(({ data }) => {
       setIsMember(data.isMember);
     });
-  });
+  },[]);
 
   return isMember;
 
