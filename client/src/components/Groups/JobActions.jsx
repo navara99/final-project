6d0b02932a64839110e03a7a890f47a03811eee7
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-22 11:43:27 -0500

    Add state for disable button for applied job

--- client/src/components/Groups/JobActions.jsx
+++ client/src/components/Groups/JobActions.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GroupsBtn from "./GroupsBtn";
 import JobApplicationForm from "./JobApplicationForm";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function JobActions({ job, isMember, setSnackBarDetails }) {
-  const [openApplicationForm, setOpenApplicationForm] = useState(false);
 
+
+function JobActions({ job, isMember, setSnackBarDetails, currentUser, isApplied, setApplied }) {
+  const [openApplicationForm, setOpenApplicationForm] = useState(false);
+  
   return (
     <>
-      <JobApplicationForm {...{ job }} {...{ openApplicationForm }} {...{ setOpenApplicationForm }} {...{ setSnackBarDetails }} />
-      {!isMember && <GroupsBtn text={"Apply"} onClick={() => setOpenApplicationForm(!openApplicationForm)} />}
+      <JobApplicationForm {...{ job }} {...{ openApplicationForm }} {...{ setOpenApplicationForm }} {...{ setSnackBarDetails }} setApplied={setApplied}/>
+      {!isMember &&  <GroupsBtn text={!isApplied? "Apply" : "Applied"} onClick={() => setOpenApplicationForm(!openApplicationForm)} deActive = {isApplied}/>}
       {isMember && <Link to={`/jobs/${job.id}/applications`} style={{ textDecoration: "none" }}><GroupsBtn text={`View Applications (${job.applications.length})`} /></Link>}
       <GroupsBtn text={"Details"} />
     </>
