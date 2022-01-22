e8f318114b6cfd593bbe0def1dbb71918ed09b1d
Siu <siuc@protonmail.com>
2022-01-21 22:18:26 -0500

    scroll to bottom

--- client/src/components/ChatBox/Messages/MessageList.jsx
+++ client/src/components/ChatBox/Messages/MessageList.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { List } from "@mui/material";
 import MessageListItem from "./MessageListItem";
 import axios from "axios";
 
 const MessageList = (props) => {
   const { messages, receiverId, setMessages } = props;
+  const myMessage = useRef(null);
+
   const messageList = messages
     ? messages.map((message, i) => (
         <MessageListItem key={i} {...{ ...props, message }} />
       ))
     : null;
+
+  useEffect(() => {
+    if (myMessage && myMessage.current) {
+      myMessage.current.scrollTop = myMessage.current.scrollHeight;
+    }
+  }, [myMessage, messages]);
+
   useEffect(() => {
     if (receiverId) {
       axios.put(`/api/messages/read/${receiverId}`).then((res) => {
@@ -22,7 +31,7 @@ const MessageList = (props) => {
       });
     }
   }, [receiverId, messages.length]);
-  return <List>{messageList}</List>;
+  return <List ref={myMessage}>{messageList}</List>;
 };
 
 export default MessageList;
