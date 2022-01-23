70d06286aca6cffb663291b76a185f27140a1eb3
Tharsikan <thar.navaratnam@gmail.com>
2022-01-22 22:03:26 -0500

    setup theme provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { MainTheme } from "./Providers/ThemeProvider";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <MainTheme>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MainTheme>,
   document.getElementById("root")
 );
