e99f676c18b2a87ef55379cecb9a445d6d521ba3
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-22 23:16:33 -0500

    Change ListItemText typography to fix error

--- client/src/components/Groups/GroupListItem.jsx
+++ client/src/components/Groups/GroupListItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { ListItemText, Card, CardActions } from "@mui/material";
+import { ListItemText, Card, Collapse, CardActions } from "@mui/material";
 import GroupAction from "./GroupAction";
 import { Link } from "react-router-dom";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import useExpand from "../../hooks/useExpand";
 
 function GroupListItem({ group, openAddMembersModal, setOpenAddMembersModal, selectedGroup, setSelectedGroup, cardStyles }) {
+  const { ExpandMore, handleExpandClick, expanded } = useExpand();
 
   return (
     <Card alignItems="flex-start" style={cardStyles}>
@@ -17,19 +19,29 @@ function GroupListItem({ group, openAddMembersModal, setOpenAddMembersModal, sel
           />
           <ListItemText
             primary={<h3 className="organization-card-name">{group.name}</h3>}
-            secondary={<p>{group.description}</p>}
+            secondary={group.description}
           />
         </Link>
         <CardActions>
-          <GroupAction
-            {...{ openAddMembersModal }}
-            {...{ setOpenAddMembersModal }}
-            {...{ group }}
-            {...{ selectedGroup }}
-            {...{ setSelectedGroup }}
-          />
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
         </CardActions>
       </div>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <GroupAction
+          {...{ openAddMembersModal }}
+          {...{ setOpenAddMembersModal }}
+          {...{ group }}
+          {...{ selectedGroup }}
+          {...{ setSelectedGroup }}
+        />
+      </Collapse>
     </Card >
   )
 };
