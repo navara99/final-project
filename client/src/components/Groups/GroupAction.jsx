38d54f61c4155ee448ee79de6fcf3ad41811a00b
Tharsikan <thar.navaratnam@gmail.com>
2022-01-22 22:36:12 -0500

    setup state and props for model

--- client/src/components/Groups/GroupAction.jsx
+++ client/src/components/Groups/GroupAction.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import GroupsBtn from "./GroupsBtn";
+import { useState } from "react";
 import { Delete, Edit } from "@mui/icons-material";
 import axios from "axios";
+import ConfirmDelete from "./ConfirmDelete";
 
-function GroupAction({ openAddMembersModal, setOpenAddMembersModal, group }) {
+function GroupAction({ group }) {
+  const [confirmModal, setConfirmModal] = useState(false);
 
   const deleteOrganization = async () => {
 
     try {
-      await axios.delete("/organizations/");
+      await axios.delete(`/organizations/${group.id}`);
     } catch (err) {
       console.log(err.message);
     }
@@ -26,7 +29,7 @@ function GroupAction({ openAddMembersModal, setOpenAddMembersModal, group }) {
       text: "Delete",
       variant: "outlined",
       icon: <Delete />,
-      onClick: deleteOrganization
+      onClick: () => setConfirmModal(!confirmModal)
     }
   ];
 
@@ -35,9 +38,17 @@ function GroupAction({ openAddMembersModal, setOpenAddMembersModal, group }) {
   });
 
   return (
-    <div className="organization-action-btns">
-      {btns}
-    </div>
+    <>
+      <ConfirmDelete {...{
+        onClick: deleteOrganization,
+        confirmModal,
+        setConfirmModal,
+        message: `Are you sure you want to delete ${group.name}?. This change is irreversible.`
+      }} />
+      <div className="organization-action-btns">
+        {btns}
+      </div>
+    </>
   );
 };
 
