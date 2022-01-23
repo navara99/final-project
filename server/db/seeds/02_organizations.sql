9f4b65d96a302439c44db36a979853faa9bdf589
Siu <siuc@protonmail.com>
2022-01-23 12:41:24 -0500

    Update 02_organizations.sql

--- server/db/seeds/02_organizations.sql
+++ server/db/seeds/02_organizations.sql
@@ -7,6 +7,11 @@ INSERT INTO organizations (
   logo
 )
 VALUES
-('Dabtype', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
-('Devcast', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'aegaegesg', 'aegaegesg', 'aegaegesg','https://blog.hubspot.com/hubfs/image8-2.jpg')
+('Newbold Compressors Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
+('Earl Entertainment Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
+('Coxon Scooters Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
+('Gilbert Training Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
+('Edwards Publications Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://www.redditinc.com/assets/images/site/reddit-logo.png'),
+('Ennis Machinery Ltd', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'aegaegesg', 'aegaegesg', 'aegaegesg','https://blog.hubspot.com/hubfs/image8-2.jpg'),
+('Vardy Lighting Services Ltd', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'aegaegesg', 'aegaegesg', 'aegaegesg','https://blog.hubspot.com/hubfs/image8-2.jpg')
 ;
