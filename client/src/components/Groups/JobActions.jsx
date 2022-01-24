8de23047a8a0a35b27275b14183a4b025850f318
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 15:32:31 -0500

    setup liketoggle function

--- client/src/components/Groups/JobActions.jsx
+++ client/src/components/Groups/JobActions.jsx
@@ -5,16 +5,27 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import axios from "axios";
 
 function JobActions({ job, isMember, setSnackBarDetails, currentUser, isApplied, setApplied }) {
   const [openApplicationForm, setOpenApplicationForm] = useState(false);
+
+  const likeToggle = async () => {
+
+    try {
+      axios.post(`api/jobs/${job.id}/like`);
+    } catch (err) {
+      console.log(err.message);
+    }
+
+  };
 
   return (
     <>
       <JobApplicationForm {...{ job }} {...{ openApplicationForm }} {...{ setOpenApplicationForm }} {...{ setSnackBarDetails }} currentUser={currentUser} setApplied={setApplied} />
       {!isMember && <GroupsBtn text={!isApplied ? "Apply" : "Applied"} onClick={() => setOpenApplicationForm(!openApplicationForm)} deActive={isApplied} />}
       {isMember && <Link to={`/jobs/${job.id}/applications`} style={{ textDecoration: "none" }}><GroupsBtn text={`View Applications (${job.applications.length})`} /></Link>}
-      <IconButton><FavoriteBorderIcon /></IconButton>
+      {!isMember && <IconButton onClick={likeToggle}><FavoriteBorderIcon /></IconButton>}
     </>
   )
 
