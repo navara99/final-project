a471ea813f652bf6ad14739c9e814062f78a789f
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 15:27:28 -0500

    add favourite btn border

--- client/src/components/Groups/JobActions.jsx
+++ client/src/components/Groups/JobActions.jsx
@@ -3,16 +3,18 @@ import GroupsBtn from "./GroupsBtn";
 import JobApplicationForm from "./JobApplicationForm";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function JobActions({ job, isMember, setSnackBarDetails, currentUser, isApplied, setApplied }) {
   const [openApplicationForm, setOpenApplicationForm] = useState(false);
-  
+
   return (
     <>
-      <JobApplicationForm {...{ job }} {...{ openApplicationForm }} {...{ setOpenApplicationForm }} {...{ setSnackBarDetails }} currentUser={currentUser}setApplied={setApplied}/>
-      {!isMember &&  <GroupsBtn text={!isApplied? "Apply" : "Applied"} onClick={() => setOpenApplicationForm(!openApplicationForm)} deActive = {isApplied}/>}
+      <JobApplicationForm {...{ job }} {...{ openApplicationForm }} {...{ setOpenApplicationForm }} {...{ setSnackBarDetails }} currentUser={currentUser} setApplied={setApplied} />
+      {!isMember && <GroupsBtn text={!isApplied ? "Apply" : "Applied"} onClick={() => setOpenApplicationForm(!openApplicationForm)} deActive={isApplied} />}
       {isMember && <Link to={`/jobs/${job.id}/applications`} style={{ textDecoration: "none" }}><GroupsBtn text={`View Applications (${job.applications.length})`} /></Link>}
-      <GroupsBtn text={"Details"} />
+      <IconButton><FavoriteBorderIcon /></IconButton>
     </>
   )
 
