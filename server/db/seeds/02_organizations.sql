51649363d590aaa414d6a53613b9d997ef3a4f31
Siu <siuc@protonmail.com>
2022-01-23 22:14:47 -0500

    add website

--- server/db/seeds/02_organizations.sql
+++ server/db/seeds/02_organizations.sql
@@ -7,16 +7,16 @@ INSERT INTO organizations (
   logo
 )
 VALUES
-('Newbold Ltd', '','newbold@mail.com','Environmental Services','@mail.com', 'https://pngimg.com/uploads/mcdonalds/mcdonalds_PNG23.png'),
-('Earl Ltd', '','earl@mail.com','Foodstuff & Beverage','@mail.com','https://pngimg.com/uploads/kfc/kfc_PNG50.png'),
-('Ennis Ltd', '', 'ennis@mail.com', 'Health Care', '@mail.com','https://pngimg.com/uploads/fifa/fifa_PNG29.png'),
-('Coxon Ltd', '','coxon@mail.com','Advertising Agency','@mail.com','https://pngimg.com/uploads/gopro_logo/gopro_logo_PNG29.png'),
-('Gilbert Ltd', '','gilbert@mail.com','Wholesale & Retail','@mail.com','https://pngimg.com/uploads/burger_king/burger_king_PNG16.png'),
-('Edwards Ltd', '','edwards@mail.com','Convention & Exhibition Services','@mail.com','https://pngimg.com/uploads/starbucks/starbucks_PNG18.png'),
-('Pinter Ltd', '','pinter@mail.com','Logistics','@mail.com','https://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG9.png'),
-('Lyons Ltd', '','lyons@mail.com','Media','@mail.com','https://pngimg.com/uploads/photoshop/photoshop_PNG42.png'),
-('Rogan Ltd', '','rogan@mail.com','Entertainment / Art / Recreation','@mail.com','https://pngimg.com/uploads/nike/nike_PNG19.png'),
-('Keats Ltd', '','keats@mail.com','Hotel & Hospitality','@mail.com','http://pngimg.com/uploads/adidas/adidas_PNG7.png'),
-('Edwards Ltd', '','edwards@mail.com','Motor Vehicles','@mail.com','https://pngimg.com/uploads/olympic_rings/olympic_rings_PNG8.png'),
-('Vardy Ltd', '', 'vardy@mail.com', 'Banking / Finance / Securities / Investments', '@mail.com','https://blog.hubspot.com/hubfs/image8-2.jpg')
+('Newbold Ltd', '','newbold@mail.com','Environmental Services','https://mondrianandme.com/', 'https://pngimg.com/uploads/mcdonalds/mcdonalds_PNG23.png'),
+('Earl Ltd', '','earl@mail.com','Foodstuff & Beverage','http://corndog.io/','https://pngimg.com/uploads/kfc/kfc_PNG50.png'),
+('Ennis Ltd', '', 'ennis@mail.com', 'Health Care', 'https://checkboxrace.com/','https://pngimg.com/uploads/fifa/fifa_PNG29.png'),
+('Coxon Ltd', '','coxon@mail.com','Advertising Agency','https://alwaysjudgeabookbyitscover.com/','https://pngimg.com/uploads/gopro_logo/gopro_logo_PNG29.png'),
+('Gilbert Ltd', '','gilbert@mail.com','Wholesale & Retail','https://thatsthefinger.com/','https://pngimg.com/uploads/burger_king/burger_king_PNG16.png'),
+('Edwards Ltd', '','edwards@mail.com','Convention & Exhibition Services','https://checkboxolympics.com/','https://pngimg.com/uploads/starbucks/starbucks_PNG18.png'),
+('Pinter Ltd', '','pinter@mail.com','Logistics','https://cant-not-tweet-this.com/','https://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG9.png'),
+('Lyons Ltd', '','lyons@mail.com','Media','https://longdogechallenge.com/','https://pngimg.com/uploads/photoshop/photoshop_PNG42.png'),
+('Rogan Ltd', '','rogan@mail.com','Entertainment / Art / Recreation','https://onesquareminesweeper.com/','https://pngimg.com/uploads/nike/nike_PNG19.png'),
+('Keats Ltd', '','keats@mail.com','Hotel & Hospitality','https://puginarug.com/','http://pngimg.com/uploads/adidas/adidas_PNG7.png'),
+('Edwards Ltd', '','edwards@mail.com','Motor Vehicles','http://eelslap.com/','https://pngimg.com/uploads/olympic_rings/olympic_rings_PNG8.png'),
+('Vardy Ltd', '', 'vardy@mail.com', 'Banking / Finance / Securities / Investments', 'http://burymewithmymoney.com/','https://blog.hubspot.com/hubfs/image8-2.jpg')
 ;
