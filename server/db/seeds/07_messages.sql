79cb900e6c527395481a6231a78460363b2a2428
Siu <siuc@protonmail.com>
2022-01-24 11:38:06 -0500

    Update 07_messages.sql

--- server/db/seeds/07_messages.sql
+++ server/db/seeds/07_messages.sql
@@ -1,13 +1,25 @@
-INSERT INTO messages (sender_id , receiver_id, message)
+INSERT INTO messages (sender_id , receiver_id, message, created_at)
 VALUES
-  (1, 2, 'Hello'),
-  (2, 1, 'Hi'),
-  (1, 2, 'How are you doing?'),
-  (2, 1, 'I am good. Thanks!'),
-  (3,2, 'Need Help?'),
-  (2,3, 'What is it about'),
-  (3,2, 'Socket io'),
-  (2,3, 'Ohh'),
-  (1,3, 'You need help?'),
-  (3,1, 'Yeah!')
+  (1, 3, 'kitsch poutine', (current_timestamp - interval '1 hour')),
+  (3, 1, 'next level vice unicorn authentic put a bird', (current_timestamp - interval '1 hour')),
+  (1, 3, 'bespoke distillery?', (current_timestamp - interval '1 hour')),
+  (3, 1, '3 wolf moon enamel pin banh mi', (current_timestamp - interval '1 hour')),
+  (1, 3, 'locavore 8-bit bicycle rights craft beer farm-to-table', (current_timestamp - interval '1 hour')),
+  (3, 1, 'Photo booth meh literally, kitsch', (current_timestamp - interval '10.5 hour')),
+  (1, 3, 'skateboard hexagon twee kale chips edison bulb vinyl four loko everyday carry ethical art party.', (current_timestamp - interval '10 hour')),
+  (3, 1, 'Paleo scenester yr echo park you probably', (current_timestamp - interval '9 hour')),
+  (1, 3, 'havent heard of them, letterpress taiyaki.', (current_timestamp - interval '8 hour')),
+  (3, 1, 'Hi', (current_timestamp - interval '7 hour')),
+  (1, 3, 'How are you doing?', (current_timestamp - interval '6 hour')),
+  (3, 1, 'I am good. Thanks!', (current_timestamp - interval '5 hour')),
+  (1, 3, 'Hello', (current_timestamp - interval '4 hour')),
+  (3, 1, 'Hi', (current_timestamp - interval '3 hour')),
+  (1, 3, 'How are you doing?', (current_timestamp - interval '2 hour')),
+  (3, 1, 'I am good. Thanks!', (current_timestamp - interval '1 hour')),
+  (2, 4, 'Need Help?', (current_timestamp - interval '1 hour')),
+  (4, 2, 'What is it about', (current_timestamp - interval '1 hour')),
+  (2, 4, 'Socket io', (current_timestamp - interval '1 hour')),
+  (4, 2, 'Ohh', (current_timestamp - interval '1 hour')),
+  (1, 3, 'You need help?', (current_timestamp - interval '1 hour')),
+  (3, 1, 'Yeah!', (current_timestamp - interval '1 hour'))
 ;
