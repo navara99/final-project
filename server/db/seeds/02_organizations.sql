5a1bf815b68f4943b41aecd694943bfff09e8ff3
Siu <siuc@protonmail.com>
2022-01-23 22:02:16 -0500

    remove description

--- server/db/seeds/02_organizations.sql
+++ server/db/seeds/02_organizations.sql
@@ -7,17 +7,17 @@ INSERT INTO organizations (
   logo
 )
 VALUES
-('Newbold Compressors Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','newbold@mail.com','aegaegesg','aegaegesg', 'https://pngimg.com/uploads/mcdonalds/mcdonalds_PNG23.png'),
-('Earl Entertainment Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','earl@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/kfc/kfc_PNG50.png'),
-('Ennis Machinery Ltd', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'ennis@mail.com', 'aegaegesg', 'aegaegesg','https://pngimg.com/uploads/fifa/fifa_PNG29.png'),
-('Coxon Scooters Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','coxon@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/gopro_logo/gopro_logo_PNG29.png'),
-('Gilbert Training Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','gilbert@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/burger_king/burger_king_PNG16.png'),
-('Edwards Publications Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/starbucks/starbucks_PNG18.png'),
-('Pinter Kitchens Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG9.png'),
-('Lyons Fitness Products Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/photoshop/photoshop_PNG42.png'),
-('Rogan Camping Supplies Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/nike/nike_PNG19.png'),
-('Keats Caterers Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','http://pngimg.com/uploads/adidas/adidas_PNG7.png'),
-('Lyons Fitness Products Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/windows_logos/windows_logos_PNG35.png'),
-('Edwards Publications Ltd', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero convallis eget eleifend luctus ultricies eu nibh.','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/olympic_rings/olympic_rings_PNG8.png'),
-('Vardy Lighting Services Ltd', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'aegaegesg', 'aegaegesg', 'aegaegesg','https://blog.hubspot.com/hubfs/image8-2.jpg')
+('Newbold Compressors Ltd', '','newbold@mail.com','aegaegesg','aegaegesg', 'https://pngimg.com/uploads/mcdonalds/mcdonalds_PNG23.png'),
+('Earl Entertainment Ltd', '','earl@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/kfc/kfc_PNG50.png'),
+('Ennis Machinery Ltd', '', 'ennis@mail.com', 'aegaegesg', 'aegaegesg','https://pngimg.com/uploads/fifa/fifa_PNG29.png'),
+('Coxon Scooters Ltd', '','coxon@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/gopro_logo/gopro_logo_PNG29.png'),
+('Gilbert Training Ltd', '','gilbert@mail.com','aegaegesg','aegaegesg','https://pngimg.com/uploads/burger_king/burger_king_PNG16.png'),
+('Edwards Publications Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/starbucks/starbucks_PNG18.png'),
+('Pinter Kitchens Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG9.png'),
+('Lyons Fitness Products Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/photoshop/photoshop_PNG42.png'),
+('Rogan Camping Supplies Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/nike/nike_PNG19.png'),
+('Keats Caterers Ltd', '','aegaegesg','aegaegesg','aegaegesg','http://pngimg.com/uploads/adidas/adidas_PNG7.png'),
+('Lyons Fitness Products Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/windows_logos/windows_logos_PNG35.png'),
+('Edwards Publications Ltd', '','aegaegesg','aegaegesg','aegaegesg','https://pngimg.com/uploads/olympic_rings/olympic_rings_PNG8.png'),
+('Vardy Lighting Services Ltd', '', 'aegaegesg', 'aegaegesg', 'aegaegesg','https://blog.hubspot.com/hubfs/image8-2.jpg')
 ;
