89e5c179d22540d3ffc0caa50d22a30688327a00
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 21:45:48 -0500

    finish useAppliedJobs hook

--- client/src/hooks/useAppliedJobs.js
+++ client/src/hooks/useAppliedJobs.js
@@ -1,13 +1,17 @@
-import React from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useAppliedJobs = () => {
   const [appliedJobs, setAppliedJobs] = useState([]);
 
   useEffect(() => {
-
+    axios.get("/api/jobs/applied").then(({ data }) => {
+      setAppliedJobs(data);
+    });
   });
 
+
+  return { appliedJobs, setAppliedJobs };
 };
 
 export default useAppliedJobs;
