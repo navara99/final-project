02d173f4ee45bc04d98e6188b6f30edcf8aebd5a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-24 20:22:05 -0500

    Style header

--- client/src/components/Groups/OrganizationHeader.jsx
+++ client/src/components/Groups/OrganizationHeader.jsx
@@ -10,41 +10,31 @@ function OrganizationHeader({ organization, cardStyles }) {
   const { ExpandMore, handleExpandClick, expanded } = useExpand();
   return (
     <div >
-      <Card style={cardStyles}>
-        <div className="organization-card">
+      <Card style={cardStyles} sx={{display:"flex", gap:"40px"}}>
+        <div >
           <img
             alt={organization.name}
-            className="organization-logo"
+            // className="organization-logo"
+            style={{height: 100, width: 100, borderRadius:'50%'}}
             src={`${organization.details.logo}`}
           />
+          <div style={{marginTop:'20px'}}>
+            <div style={{display:"flex", alignSelf:"center",marginTop:"10px"}}>
+              <BusinessIcon fontSize ="large" />  <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}>{organization.details.industry}</Typography>
+            </div>
+            <div style={{display:"flex", alignSelf:"center",marginTop:"10px"}}>
+              <EmailIcon fontSize ="large" /> <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}>{organization.details.email}</Typography>
+            </div>
+            <div style={{display:"flex", alignSelf:"center", marginTop:"10px"}}>
+              <LanguageIcon fontSize ="large" />  <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}>{organization.details.website}</Typography>
+            </div>
+          </div>
+          </div>
           <ListItemText
             primary={<h2 className="organization-card-name">{organization.details.name}</h2>}
             secondary={organization.details.description}
-          />
-          <CardActions>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-        </div>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <div style={{marginTop:'20px'}}>
-            <div style={{display:"flex", alignSelf:"center",marginTop:"10px"}}>
-              <BusinessIcon fontSize ="large" />  <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}> Industry : {organization.details.industry}</Typography>
-            </div>
-            <div style={{display:"flex", alignSelf:"center",marginTop:"10px"}}>
-              <EmailIcon fontSize ="large" /> <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}>  Email : {organization.details.email}</Typography>
-            </div>
-            <div style={{display:"flex", alignSelf:"center", marginTop:"10px"}}>
-              <LanguageIcon fontSize ="large" />  <Typography component="span"  color={blueGrey["A700"]} variant="subtitle1"  sx={{ml:1, alignSelf:"center"}}> Website : {organization.details.website}</Typography>
-            </div>
-          </div>
-        </Collapse>
+          />  
+
       </Card >
     </div>
   )
