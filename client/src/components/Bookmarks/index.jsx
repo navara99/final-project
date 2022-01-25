b90c96386126dc703fc2cba683fbea462c10bdb6
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 21:47:48 -0500

    call useAppliedJobs hook

--- client/src/components/Bookmarks/index.jsx
+++ client/src/components/Bookmarks/index.jsx
@@ -3,10 +3,14 @@ import Columns from "./Columns";
 import { useState } from "react";
 import useFavoriteJobs from "../../hooks/useFavoriteJobs";
 import JobsList from "../Groups/JobsList";
+import useAppliedJobs from "../../hooks/useAppliedJobs";
 
 function Bookmarks() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const { favoriteJobs, setFavoriteJobs } = useFavoriteJobs();
+  const { appliedJobs, setAppliedJobs } = useAppliedJobs();
+
+  console.log(appliedJobs);
 
   return (
     <>
