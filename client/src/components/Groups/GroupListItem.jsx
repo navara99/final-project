c4e4208937c8075c1e558e5308250d2572b1ad6a
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-25 16:22:22 -0500

    Remove CardAction import

--- client/src/components/Groups/GroupListItem.jsx
+++ client/src/components/Groups/GroupListItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListItemText, Card, CardActions, CardActionArea } from "@mui/material";
+import { ListItemText, Card, CardActions } from "@mui/material";
 import GroupAction from "./GroupAction";
 import { Link } from "react-router-dom";
 import { useState } from "react";
