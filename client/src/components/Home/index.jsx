1e845400f9de87862811d78d2fa18d4e3a1d335b
Siu <siuc@protonmail.com>
2022-01-24 22:32:14 -0500

    useTitle for home page

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import useFairs from "../../hooks/useFairs";
 import "./Home.css";
 import FairList from "./FairList";
+import useTitle from "../../hooks/useTitle";
 
 const Home = () => {
   const { onGoingFairs, upcomingFairs, pastFairs } = useFairs();
+  useTitle("Home");
 
   return (
     <div className="Home">
-      <FairList items={onGoingFairs} text="Career Fairs LIVE" isOngoing={true} />
+      <FairList
+        items={onGoingFairs}
+        text="Career Fairs LIVE"
+        isOngoing={true}
+      />
       <FairList
         items={upcomingFairs}
         text="Upcoming Career Fairs"
         showDate={true}
       />
-      <FairList items={pastFairs} text="Past Career Fairs" showDate={true} /> 
+      <FairList items={pastFairs} text="Past Career Fairs" showDate={true} />
     </div>
   );
 };
