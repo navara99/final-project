1d4d78ec04c9a4f4df9803b303498e2dca15f711
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 21:42:54 -0500

    implement job list component with new favorites array

--- client/src/components/Bookmarks/index.jsx
+++ client/src/components/Bookmarks/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Columns from "./Columns";
 import { useState } from "react";
+import useFavoriteJobs from "../../hooks/useFavoriteJobs";
+import JobsList from "../Groups/JobsList";
 
 function Bookmarks() {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const { favoriteJobs, setFavoriteJobs } = useFavoriteJobs();
 
   return (
     <>
       <Columns {...{ selectedIndex, setSelectedIndex }} />
+      {selectedIndex === 0 && <JobsList jobs={favoriteJobs} />}
     </>
   )
 };
