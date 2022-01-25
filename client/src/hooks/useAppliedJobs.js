27f333513781aeb6429130e72b99e6fa6c5861ad
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 21:44:42 -0500

    setup useAppliedJobs hook

---
+++ client/src/hooks/useAppliedJobs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const useAppliedJobs = () => {
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  useEffect(() => {
+
+  });
+
+};
+
+export default useAppliedJobs;
