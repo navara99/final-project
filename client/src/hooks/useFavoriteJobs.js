6610a945663c4d01e05efb139eccf7fb6e9f990f
Tharsikan <thar.navaratnam@gmail.com>
2022-01-24 21:23:47 -0500

    finish useFavoriteJobs hook

--- client/src/hooks/useFavoriteJobs.js
+++ client/src/hooks/useFavoriteJobs.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 const useFavoriteJobs = () => {
+  const [favoriteJobs, setFavoriteJobs] = useState([]);
 
-  
+  useEffect(() => {
+    axios.get("/api/jobs/favorite").then(({ data }) => {
+      setFavoriteJobs(data);
+    });
+  }, []);
 
+  return { favoriteJobs, setFavoriteJobs };
 
 };
 
