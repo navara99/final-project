6e9431e3381e3c9e7c36bd227efb97ae8d18d6be
Tharsikan <thar.navaratnam@gmail.com>
2022-01-25 16:54:33 +0000

    add axios base url

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { MainTheme } from "./Providers/ThemeProvider";
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+};
 
 ReactDOM.render(
   <MainTheme>
